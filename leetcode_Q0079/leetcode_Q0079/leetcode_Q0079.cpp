5d1e54277e793554f470f6c3c777947f4b897394
Kazuho Sakaoka <kazuho-sakaoka@outlook.jp>
2024-01-07 13:59:46 +0900

    Q0122

---
+++ leetcode_Q0079/leetcode_Q0079/leetcode_Q0079.cpp
@@ -0,0 +1,71 @@
+﻿// leetcode_Q0079.cpp : このファイルには 'main' 関数が含まれています。プログラム実行の開始と終了がそこで行われます。
+//
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+
+    bool exist_rec(vector<vector<char>>& board, string word, int index, const pair<int,int>& currentPoint, vector<vector<bool>>& used) {
+
+        if (board[currentPoint.first][currentPoint.second] != word[index]) {
+            used[currentPoint.first][currentPoint.second] = false;
+            return false;
+        }
+        if (word.size() - 1 == index) { return true; }
+
+        used[currentPoint.first][currentPoint.second] = true;
+
+        static const pair<int, int> directions[]
+        {
+            {-1,0},{1,0}, {0,-1,},{0,1},
+        };
+
+        for (auto&& direction : directions) {
+            const auto current = make_pair(currentPoint.first + direction.first, currentPoint.second + direction.second);
+            if (current.first < 0 || board.size() <= current.first) { continue; }
+            if (current.second < 0 || board.front().size() <= current.second) { continue; }
+            if (used[current.first][current.second]) { continue; }
+            if (exist_rec(board, word, index + 1, current, used)) {
+                return true;
+            }
+        }
+        used[currentPoint.first][currentPoint.second] = false;
+        return false;
+    }
+
+
+    bool exist(vector<vector<char>>& board, string word) {
+        vector<vector<bool>> used(board.size(), vector<bool>(board.front().size(), false));
+
+        for (int i = 0; i < board.size(); i++) {
+            for (int j = 0; j < board.front().size(); j++) {
+                if (exist_rec(board, word, 0, make_pair(i, j), used)) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+};
+
+
+void test(vector<vector<char>>&& board, const string& word) {
+    cout << Solution().exist(board, word) << endl;
+}
+
+
+int main()
+{
+    test({ {'a','a'} }, "aaa");
+
+    test({ {'A',} }, "A");
+
+    test({ {'A', 'B', 'C', 'E'}, { 'S','F','C','S' }, { 'A','D','E','E' } }, "ABCCED");
+    test({ {'A', 'B', 'C', 'E'}, { 'S','F','C','S' }, { 'A','D','E','E' } }, "SEE");
+    test({ {'A', 'B', 'C', 'E'}, { 'S','F','C','S' }, { 'A','D','E','E' } }, "ABCB");
+}
---
+++ leetcode_Q0122/leetcode_Q0122/leetcode_Q0122.cpp
@@ -0,0 +1,52 @@
+﻿// leetcode_Q0122.cpp : このファイルには 'main' 関数が含まれています。プログラム実行の開始と終了がそこで行われます。
+//
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        if (prices.size() < 2) { return 0; }
+        int total = 0;
+        bool holding = false;
+        int before = prices[0];
+        int floor_val = INT_MAX;
+
+        for (int i = 1; i < prices.size(); i++) {
+            const auto diff = prices[i] - before;
+            if (!holding) {
+                if (diff > 0) {
+                    holding = true;
+                    floor_val = before;
+                }
+            }
+            else {
+                if (diff < 0) {
+                    holding = false;
+                    total += before - floor_val;
+                }
+            }
+            before = prices[i];
+        }
+
+        if (holding && floor_val < prices.back())
+        {
+            total += prices.back() - floor_val;
+        }
+        return total;
+    }
+};
+
+void test(vector<int>&& prices) {
+    cout << Solution().maxProfit(prices) << endl;
+}
+
+int main()
+{
+    test({ 7,1,5,3,6,4 });
+    test({ 1,2,3,4,5 });
+    test({ 7,6,4,3,1 });
+}
