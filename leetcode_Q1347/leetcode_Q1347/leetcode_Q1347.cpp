857e2cb33bfae367a425c220ac4de25675741e4d
Kazuho Sakaoka <kazuho-sakaoka@outlook.jp>
2024-01-13 20:52:11 +0900

    Q1347

---
+++ leetcode_Q1347/leetcode_Q1347/leetcode_Q1347.cpp
@@ -0,0 +1,44 @@
+﻿// leetcode_Q1347.cpp : このファイルには 'main' 関数が含まれています。プログラム実行の開始と終了がそこで行われます。
+//
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <map>
+
+using namespace std;
+
+class Solution {
+public:
+    int minSteps(string s, string t) {
+
+        map<char, int> map;
+        for (int i = 0; i < s.size(); i++) {
+            map[s[i]]++;
+        }
+
+        int count = 0;
+        for (int i = 0; i < t.size(); i++) {
+            if (map[t[i]] == 0) {
+                count++;
+            }
+            else {
+                map[t[i]]--;
+            }
+           
+        }
+        return count;
+    }
+};
+
+void test(string&& s, string&& t) {
+    cout << Solution().minSteps(s, t) << endl;
+}
+
+
+int main()
+{
+    test("bab", "aba");
+    test("leetcode", "practice");
+    test("anagram", "mangaar");
+}
