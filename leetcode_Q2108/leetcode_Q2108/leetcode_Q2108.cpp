76b84002458bcdc59cbff687abd48008c3045110
Kazuho Sakaoka <kazuho-sakaoka@outlook.jp>
2024-02-14 22:03:23 +0900

    Q2108

---
+++ leetcode_Q2108/leetcode_Q2108/leetcode_Q2108.cpp
@@ -0,0 +1,45 @@
+﻿// leetcode_Q2108.cpp : このファイルには 'main' 関数が含まれています。プログラム実行の開始と終了がそこで行われます。
+//
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    static bool is_palindrome(const string& word) {
+        for (size_t i = 0; i < word.size() / 2; i++) {
+            if (word[i] != word[word.size() - i - 1]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+    string firstPalindrome(vector<string>& words) {
+        for (auto&& word : words) {
+            if (is_palindrome(word)) {
+                return word;
+            }
+        }
+        return "";
+    }
+};
+
+void test(vector<string>&& words) {
+    cout << Solution().firstPalindrome(words) << endl;
+}
+
+
+int main()
+{
+    test({ "abc","car","ada","racecar","cool" });
+    test({ "notapalindrome","racecar" });
+    test({ "def","ghi" });
+    test({ "qac","aa" });
+
+    test({ "d","aa" });
+}
---
+++ leetcode_Q2149/leetcode_Q2149/leetcode_Q2149.cpp
@@ -0,0 +1,77 @@
+﻿// leetcode_Q2149.cpp : このファイルには 'main' 関数が含まれています。プログラム実行の開始と終了がそこで行われます。
+//
+
+#include <iostream>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> rearrangeArray(vector<int>& nums) {
+        queue<int> positive_queue;
+        queue<int> negative_queue;
+        vector<int> ans;
+        ans.reserve(nums.size());
+        bool positive = true;
+
+        for (auto&& value : nums) {
+            if (value < 0) {
+                negative_queue.push(value);
+            }
+            else {
+                positive_queue.push(value);
+            }
+
+            for (;;) {
+                if (positive && !positive_queue.empty()) {
+                    ans.emplace_back(positive_queue.front());
+                    positive_queue.pop();
+                    positive = false;
+                }
+                else if (!positive && !negative_queue.empty()) {
+                    ans.emplace_back(negative_queue.front());
+                    negative_queue.pop();
+                    positive = true;
+                }
+                else {
+                    break;
+                }
+            }
+        }
+
+        while (!positive_queue.empty() || !negative_queue.empty())
+        {
+            if (positive) {
+                ans.emplace_back(positive_queue.front());
+                positive_queue.pop();
+                positive = false;
+            }
+            else {
+                ans.emplace_back(negative_queue.front());
+                negative_queue.pop();
+                positive = true;
+            }
+        }
+
+        return ans;
+    }
+};
+
+void test(vector<int>&& nums) {
+    const auto ans = Solution().rearrangeArray(nums);
+    for (auto&& value : ans) {
+        cout << value << ",";
+    }
+    cout << endl;
+}
+
+
+int main()
+{
+    test({ 3,1,-2,-5,2,-4 });
+    test({ -1,1 });
+
+    test({ -1,-2,-3,-4, 4,3,2,1 });
+}
