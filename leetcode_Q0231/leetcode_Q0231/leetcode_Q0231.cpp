f243c3c744d5e9d9f2145da6e4ceb654d747ce16
Kazuho Sakaoka <kazuho-sakaoka@outlook.jp>
2024-02-19 21:32:19 +0900

    Q0231

---
+++ leetcode_Q0231/leetcode_Q0231/leetcode_Q0231.cpp
@@ -0,0 +1,33 @@
+﻿// leetcode_Q0231.cpp : このファイルには 'main' 関数が含まれています。プログラム実行の開始と終了がそこで行われます。
+//
+
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+
+
+    bool isPowerOfTwo(int n) {
+        if (n < 0) { return false; }
+        for (int i = 0; i < 32; i++) {
+            if (n == (1 << i)) {
+                return true;
+            }
+        }
+        return false;
+    }
+};
+
+
+void test(int n) {
+    cout << (Solution().isPowerOfTwo(n) ? "true" : "false") << endl;
+}
+
+int main(){
+    test(1);
+    test(16);
+    test(3);
+    test(pow(2, 31));
+}
