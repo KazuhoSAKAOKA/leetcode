c23c4cd95168c4afd9396b96f46833393ebb2893
Kazuho Sakaoka <kazuho-sakaoka@outlook.jp>
2024-03-14 21:56:34 +0900

    Q930 TLE

---
+++ leetcode_Q0930/leetcode_Q0930/leetcode_Q0930.cpp
@@ -0,0 +1,34 @@
+﻿// leetcode_Q0930.cpp : このファイルには 'main' 関数が含まれています。プログラム実行の開始と終了がそこで行われます。
+//
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int numSubarraysWithSum(vector<int>& nums, int goal) {
+        int total = 0;
+        for (auto i = 0; i < nums.size(); i++) {
+            int sum = 0;
+            for (int j = 0; i + j < nums.size(); j++) {
+                sum += nums[i + j];
+                if (sum == goal) {
+                    total++;
+                }
+            }
+        }
+        return total;
+    }
+};
+
+void test(vector<int>&& nums, int goal) {
+    cout << Solution().numSubarraysWithSum(nums, goal) << endl;
+}
+
+int main()
+{
+    test({ 1,0,1,0,1 }, 2);
+    test({ 0,0,0,0,0 }, 0);
+}
---
+++ leetcode_Q2485/leetcode_Q2485/leetcode_Q2485.cpp
@@ -0,0 +1,37 @@
+﻿// leetcode_Q2485.cpp : このファイルには 'main' 関数が含まれています。プログラム実行の開始と終了がそこで行われます。
+//
+
+#include <iostream>
+
+using namespace std;
+
+
+class Solution {
+public:
+
+    static long total(int begin, int include_end) {
+        return (begin + include_end) * (include_end - begin + 1) / 2;
+    }
+
+    int pivotInteger(int n) {
+
+        for (int i = 1; i <= n; i++) {
+            if (total(1, i) == total(i, n)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+};
+
+void test(int n) {
+    cout << Solution().pivotInteger(n) << endl;
+}
+
+
+int main()
+{
+    test(8);
+    test(1);
+    test(4);
+}
