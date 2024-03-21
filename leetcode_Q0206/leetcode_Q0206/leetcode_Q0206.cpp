70cf8ef338d9808a7c6ac24358e5d13082f8444a
Kazuho Sakaoka <kazuho-sakaoka@outlook.jp>
2024-03-21 21:20:11 +0900

    Q206

---
+++ leetcode_Q0206/leetcode_Q0206/leetcode_Q0206.cpp
@@ -0,0 +1,39 @@
+﻿// leetcode_Q0206.cpp : このファイルには 'main' 関数が含まれています。プログラム実行の開始と終了がそこで行われます。
+//
+
+#include <iostream>
+#include <vector>
+#include "./../../utility/ListNodeUtil.h"
+
+using namespace std;
+
+
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        if (head == nullptr) { return nullptr; }
+        ListNode* prev = head;
+        ListNode* current = head->next;
+        prev->next = nullptr;
+        while (current != nullptr) {
+            ListNode* next = current->next;
+            current->next = prev;
+            prev = current;
+            current = next;
+        }
+        return prev;
+    }
+};
+
+void test(vector<int>&& values) {
+    output_listnode(Solution().reverseList(create_listnode(values)));
+}
+
+
+int main()
+{
+    test({ 1,2, });
+    test({ 1,2,3, });
+    test({ 1, });
+    test({});
+}
