0bbf2239f8bf4b8d5f633a460ff29e121b782d9e
Kazuho Sakaoka <kazuho-sakaoka@outlook.jp>
2024-04-04 20:02:29 +0900

    Q1614

---
+++ leetcode_Q0205/leetcode_Q0205/leetcode_Q0205.cpp
@@ -0,0 +1,50 @@
+﻿// leetcode_Q0205.cpp : このファイルには 'main' 関数が含まれています。プログラム実行の開始と終了がそこで行われます。
+//
+
+#include <iostream>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+
+using namespace std;
+
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        if (s.size() != t.size()) {
+            return false;
+        }
+
+        unordered_map<char, char> convert_map;
+        unordered_set<char> reverse_set;
+
+        for (int i = 0; i < s.size(); i++) {
+            const auto it = convert_map.find(s[i]);
+            if (it == cend(convert_map)) {
+                if (reverse_set.find(t[i]) != cend(reverse_set)) {
+                    return false;
+                }
+                convert_map.insert(make_pair(s[i], t[i]));
+                reverse_set.insert(t[i]);
+            }
+            else {
+                if (it->second != t[i]) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+};
+
+void test(string&& s, string&& t) {
+    cout << Solution().isIsomorphic(s, t) << endl;
+}
+
+int main()
+{
+    test("egg", "add");
+    test("foo", "bar");
+    test("paper", "title");
+}
---
+++ leetcode_Q1614/leetcode_Q1614/leetcode_Q1614.cpp
@@ -0,0 +1,38 @@
+﻿// leetcode_Q1614.cpp : このファイルには 'main' 関数が含まれています。プログラム実行の開始と終了がそこで行われます。
+//
+
+#include <iostream>
+#include <string>
+#include <stack>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    int maxDepth(string s) {
+        stack<char> parents;
+        int max_depth = 0;
+        for (auto&& c : s) {
+            if (c == '(') {
+                parents.push(c);
+                max_depth = max(max_depth, static_cast<int>(parents.size()));
+            }
+            else if (c == ')') {
+                parents.pop();
+            }
+
+        }
+        return max_depth;
+    }
+};
+
+void test(string&& s) {
+    cout << Solution().maxDepth(s) << endl;
+}
+
+int main()
+{
+    test("(1+(2*3)+((8)/4))+1");
+    test("(1)+((2))+(((3)))");
+}
