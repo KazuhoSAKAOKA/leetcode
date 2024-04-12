b9a35dbea8c950414215baaaf5abf5cbded04c0f
Kazuho Sakaoka <kazuho-sakaoka@outlook.jp>
2024-04-12 23:11:27 +0900

    Q42

---
+++ leetcode_Q0042/leetcode_Q0042/leetcode_Q0042.cpp
@@ -0,0 +1,53 @@
+﻿// leetcode_Q0042.cpp : このファイルには 'main' 関数が含まれています。プログラム実行の開始と終了がそこで行われます。
+//
+
+#include <iostream>
+#include <vector>
+#include <stack>
+
+using namespace std;
+
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        if (height.empty()) { return 0; }
+        int total = 0;
+        stack<int> height_stack;
+        int current_height = height[0];
+
+        for (int i = 1; i < height.size(); i++) {
+            if (height[i] == current_height) {
+                continue;
+            }
+            if (current_height < height[i]) {
+                height_stack.push(i - 1);
+                while (!height_stack.empty() && height[height_stack.top()] <= height[i]) {
+                    const int trapped_height = min(height[i], height[height_stack.top()]);
+                    total += (trapped_height - current_height) * (i - height_stack.top() - 1);
+                    current_height = height[height_stack.top()];
+                    height_stack.pop();
+                }
+                if (!height_stack.empty()) {
+                    const int trapped_height = min(height[i], height[height_stack.top()]);
+                    total += (trapped_height - current_height) * (i - height_stack.top() - 1);
+                }
+            }
+            else {
+                height_stack.push(i - 1);
+            }
+            current_height = height[i];
+        }
+        return total;
+    }
+};
+
+void test(vector<int>&& height) {
+    cout << Solution().trap(height) << endl;
+}
+
+int main()
+{
+    test({ 5,5,1,7,1,1,5,2,7,6 });
+    test({ 0,1,0,2,1,0,1,3,2,1,2,1,});
+    test({ 4,2,0,3,2,5 });
+}
---
+++ leetcode_Q0402/leetcode_Q0402/leetcode_Q0402.cpp
@@ -0,0 +1,43 @@
+﻿// leetcode_Q0402.cpp : このファイルには 'main' 関数が含まれています。プログラム実行の開始と終了がそこで行われます。
+//
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+
+class Solution {
+public:
+    string removeKdigits(string num, int k) {
+        string ans;
+
+        auto it = cbegin(num);
+        auto it_e = next(it, k);
+
+        for (; it_e != cend(num);) {
+            const auto it1 = min_element(it, next(it_e));
+            if (!(ans.empty() && *it1 == '0')) {
+                ans.push_back(*it1);
+            }
+            it = next(it1);
+            ++it_e;
+        }
+
+        if (ans.empty()) {
+            ans.push_back('0');
+        }
+        return ans;
+    }
+};
+void test(string&& num, int k) {
+    cout << Solution().removeKdigits(num, k) << endl;
+}
+int main()
+{
+    test("1432219", 3);
+    test("10200", 1);
+    test("10", 2);
+}
