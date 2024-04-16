ebed8e36ca217747f2f1373c21657604fc9ebadf
Kazuho Sakaoka <kazuho-sakaoka@outlook.jp>
2024-04-16 22:17:29 +0900

    Q623　境界条件見落とし

---
+++ leetcode_Q0623/leetcode_Q0623/leetcode_Q0623.cpp
@@ -0,0 +1,52 @@
+﻿// leetcode_Q0623.cpp : このファイルには 'main' 関数が含まれています。プログラム実行の開始と終了がそこで行われます。
+//
+
+#include <iostream>
+#include <vector>
+#include "./../../utility/TreeNodeUtil.h"
+
+using namespace std;
+
+class Solution {
+public:
+    static TreeNode* add_row(TreeNode* node, int val, int depth, bool left) {
+        if (node == nullptr) {
+            if (depth == 0) {
+                return new TreeNode(val);
+            }
+            else {
+                return nullptr;
+            }
+        }
+        if (depth == 0) {
+            auto* new_node = new TreeNode(val);
+            if (left) {
+                new_node->left = node;
+            }
+            else {
+                new_node->right = node;
+            }
+            return new_node;
+        }
+        node->left = add_row(node->left, val, depth - 1, true);
+        node->right = add_row(node->right, val, depth - 1, false);
+        return node;
+    }
+
+    TreeNode* addOneRow(TreeNode* root, int val, int depth) {
+        return add_row(root, val, depth - 1, true);
+    }
+};
+
+void test(vector<optional<int>>&& values, int val, int depth) {
+    auto* root = create_treenode(values);
+    auto* ans = Solution().addOneRow(root, val, depth);
+    output_treenode(ans);
+}
+
+int main()
+{
+    test({ 4,2,6,3,1,5, }, 1, 1);
+    test({ 4,2,6,3,1,5, }, 1, 2);
+    test({ 4,2,nullopt, 3,1, }, 1, 3);
+}
