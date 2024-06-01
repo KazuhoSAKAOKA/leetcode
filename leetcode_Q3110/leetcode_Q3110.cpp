a27591ed46cbf2b760c7b557762a35e5c65645c4
Kazuho Sakaoka <kazuho-sakaoka@outlook.jp>
2024-06-01 10:09:19 +0900

    Q3110

---
+++ leetcode_Q3110/leetcode_Q3110.cpp
@@ -0,0 +1,27 @@
+﻿// leetcode_Q3110.cpp : このファイルには 'main' 関数が含まれています。プログラム実行の開始と終了がそこで行われます。
+//
+
+#include <iostream>
+#include <string>
+#include <cmath>
+using namespace std;
+
+class Solution {
+public:
+    int scoreOfString(string s) {
+        int result = 0;
+        for (int i = 1; i < s.size(); i++) {
+            result += abs(static_cast<int>(s[i - 1]) - static_cast<int>(s[i]));
+        }
+        return result;
+    }
+};
+
+void test(string&& s) {
+    cout << Solution().scoreOfString(s) << endl;
+}
+int main()
+{
+    test("hello");
+    test("zaz");
+}
