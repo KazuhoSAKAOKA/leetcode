7c0bc11eb0961b61b7c54f85dc27444a320c9a4e
Kazuho Sakaoka <kazuho-sakaoka@outlook.jp>
2024-07-06 09:51:59 +0900

    Q2582

---
+++ leetcode_Q2582/leetcode_Q2582.cpp
@@ -0,0 +1,30 @@
+﻿// leetcode_Q2582.cpp : このファイルには 'main' 関数が含まれています。プログラム実行の開始と終了がそこで行われます。
+//
+
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    int passThePillow(int n, int time) {
+        const int cycle = n * 2 - 2;
+        const int m = time % cycle;
+        if (m < n) {
+            return m + 1;
+        }
+        else {
+            return (n - 1) - (m - n);
+        }
+    }
+};
+
+void test(int n, int time) {
+    cout << Solution().passThePillow(n, time) << endl;
+}
+
+int main()
+{
+    test(4, 5);
+    test(3, 2);
+}
