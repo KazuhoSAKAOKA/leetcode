c3e03fe952dc7fd382207ecb88602dd0dd89e1f5
Kazuho Sakaoka <kazuho-sakaoka@outlook.jp>
2024-07-07 09:40:49 +0900

    Q1518

---
+++ leetcode_Q1518/leetcode_Q1518.cpp
@@ -0,0 +1,35 @@
+﻿// leetcode_Q1518.cpp : このファイルには 'main' 関数が含まれています。プログラム実行の開始と終了がそこで行われます。
+//
+
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    int numWaterBottles(int numBottles, int numExchange) {
+        if (numExchange == 0) {
+            return 0;
+        }
+        int count = 0;
+        int current_bottles = numBottles;
+        int empty_bottles = 0;
+        while (current_bottles > 0) {
+            count += current_bottles;
+            empty_bottles += current_bottles;
+            current_bottles = empty_bottles / numExchange;
+            empty_bottles %= numExchange;
+        }
+
+        return count;
+    }
+};
+
+void test(int numBottles, int numExchange) {
+    cout << Solution().numWaterBottles(numBottles, numExchange) << endl;
+}
+int main()
+{
+    test(9, 3);
+    test(15, 4);
+}
