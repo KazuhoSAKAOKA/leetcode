7aea47014d7a9380ee6a0d71fa1e2ae9bff90fd4
Kazuho Sakaoka <kazuho-sakaoka@outlook.jp>
2024-07-10 09:10:21 +0900

    Q1598

---
+++ leetcode_Q1598/leetcode_Q1598.cpp
@@ -0,0 +1,40 @@
+﻿// leetcode_Q1598.cpp : このファイルには 'main' 関数が含まれています。プログラム実行の開始と終了がそこで行われます。
+//
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include "./../utility/leetcode_testcase_helper.h"
+
+using namespace std;
+
+class Solution {
+public:
+    int minOperations(vector<string>& logs) {
+        int depth = 0;
+
+        for (auto&& op : logs) {
+            if (op == "../") {
+                depth = max(0, depth - 1);
+            }
+            else if (op == "./") {
+
+            }
+            else {
+                depth++;
+            }
+        }
+        return depth;
+    }
+};
+
+void test(vector<string>&& logs) {
+    cout << Solution().minOperations(logs) << endl;
+}
+int main()
+{
+    test(get_list_str(R"(["d1/","d2/","../","d21/","./"])"));
+    test(get_list_str(R"(["d1/","d2/","./","d3/","../","d31/"])"));
+    test(get_list_str(R"(["d1/","../","../","../"])"));
+    return 0;
+}
---
+++ leetcode_Q1701/leetcode_Q1701.cpp
@@ -0,0 +1,40 @@
+﻿// leetcode_Q1701.cpp : このファイルには 'main' 関数が含まれています。プログラム実行の開始と終了がそこで行われます。
+//
+
+#include <iostream>
+#include <vector>
+
+#include "./../utility/leetcode_testcase_helper.h"
+
+using namespace std;
+
+class Solution {
+public:
+    double averageWaitingTime(vector<vector<int>>& customers) {
+        if (customers.empty()) {
+            return 0.0;
+        }
+        long long total_wait = 0;
+        int current_time = 0;
+        for (auto&& values : customers) {
+            const int arrive = values[0];
+            const int time = values[1];
+            const int begin = max(current_time, arrive);
+            const int wait = begin + time - arrive;
+            total_wait += static_cast<long long>(wait);
+            current_time = begin + time;
+        }
+
+        return static_cast<double>(total_wait) / static_cast<double>(customers.size());
+    }
+};
+
+void test(vector<vector<int>>&& customers) {
+    cout << Solution().averageWaitingTime(customers) << endl;
+}
+
+int main() {
+    test(get_matrix_int("[[1,2],[2,5],[4,3]]"));
+    test(get_matrix_int("[[5,2],[5,4],[10,3],[20,1]]"));
+    return 0;
+}
