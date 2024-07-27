db1c249ebfd480d8ea75d27c1a69941a0120b359
Kazuho Sakaoka <kazuho-sakaoka@outlook.jp>
2024-07-27 10:36:30 +0900

    Q2976

---
+++ leetcode_Q2976/leetcode_Q2976.cpp
@@ -0,0 +1,84 @@
+﻿// leetcode_Q2976.cpp : このファイルには 'main' 関数が含まれています。プログラム実行の開始と終了がそこで行われます。
+//
+
+#include <iostream>
+#include <vector>
+#include <minmax.h>
+#include <queue>
+
+#include "./../utility/leetcode_testcase_helper.h"
+
+using namespace std;
+
+constexpr long long INF = LLONG_MAX;
+constexpr size_t SIZE = 'z' - 'a' + 1;
+
+class Solution {
+public:
+	struct path {
+		int cost_;
+		char to_;
+	};
+
+	static vector<long long> djikstra(const vector<vector<path>>& graph, char s) {
+		vector<long long> costs(SIZE, INF);
+		struct point {
+			long long cost;
+			char pos;
+			bool operator > (const point& other) const {
+				return cost > other.cost;
+			}
+		};
+		std::priority_queue<point, vector<point>, greater<point>> p_queue;
+		p_queue.push(point{ 0, s });
+		costs[s] = 0;
+		while (!p_queue.empty()) {
+			const point current = p_queue.top();
+			p_queue.pop();
+			if (costs[current.pos] < current.cost) { continue; }
+			for (auto&& p : graph[static_cast<size_t>(current.pos)]) {
+				if (costs[p.to_] > p.cost_ + current.cost) {
+					costs[p.to_] = p.cost_ + current.cost;
+					p_queue.push(point{ costs[p.to_] , p.to_ });
+				}
+			}
+		}
+		return costs;
+	}
+
+    long long minimumCost(string source, string target, vector<char>& original, vector<char>& changed, vector<int>& cost) {
+
+		vector<vector<path>> graph(SIZE);
+		for (int i = 0; i < original.size(); i++) {
+			graph[original[i] - 'a'].push_back({cost[i], static_cast<char>(changed[i] - 'a') });
+		}
+
+		vector<vector<long long>> min_costs;
+		min_costs.reserve(SIZE);
+		for (int i = 0; i < SIZE; i++) {
+			min_costs.emplace_back(djikstra(graph, static_cast<char>(i)));
+		}
+
+        long long total = 0;
+        for (size_t i = 0; i < source.size(); i++) {
+            const auto cost = min_costs[source[i] - 'a'][target[i] - 'a'];
+            if (cost == INF) { return -1; }
+            total += cost;
+        }
+        return total;
+    }
+};
+
+void test(string&& source, string&& target, vector<char>&& original, vector<char>&& changed, vector<int>&& cost) {
+    cout << Solution().minimumCost(source, target, original, changed, cost) << endl;
+}
+
+
+int main()
+{
+    test("abcd", "acbe", get_list_char(R"(["a","b","c","c","e","d"])"), get_list_char(R"(["b","c","b","e","b","e"])"), get_list_int("[2,5,5,1,2,20]"));
+    test("aaaa", "bbbb", get_list_char(R"(["a","c"])"), get_list_char(R"(["c","b"])"), get_list_int("[1,2]"));
+    test("abcd", "abce", get_list_char(R"(["a"])"), get_list_char(R"(["e"])"), get_list_int("[10000]"));
+
+    return 0;
+}
