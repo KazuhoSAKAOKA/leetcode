142040f0fc67dc26d2059bc483cea02c32d537e6
Kazuho Sakaoka <kazuho-sakaoka@outlook.jp>
2024-08-01 10:35:51 +0900

    Q2678

---
+++ leetcode_Q2678/leetcode_Q2678.cpp
@@ -0,0 +1,35 @@
+﻿// leetcode_Q2678.cpp : このファイルには 'main' 関数が含まれています。プログラム実行の開始と終了がそこで行われます。
+//
+
+#include <iostream>
+#include <vector>
+#include <string>
+
+#include "./../utility/leetcode_testcase_helper.h"
+
+using namespace std;
+class Solution {
+public:
+    int countSeniors(vector<string>& details) {
+        int count = 0;
+        for (auto&& detail : details) {
+            const auto age_part = detail.substr(11, 2);
+            const auto age = stoi(age_part);
+            if (60 < age) {
+                count++;
+            }
+        }
+        return count;
+    }
+};
+
+void test(vector<string>&& details) {
+    cout << Solution().countSeniors(details) << endl;
+}
+
+int main()
+{
+    test(get_list_str(R"(["7868190130M7522","5303914400F9211","9273338290F4010"])"));
+    test(get_list_str(R"(["1313579440F2036","2921522980M5644"])"));
+    return 0;
+}
