112e45e364fce3bdaa8ddc33a24a8bbdb29d1a7b
Kazuho Sakaoka <kazuho-sakaoka@outlook.jp>
2024-09-13 11:30:41 +0900

    Q1310,1684

---
+++ leetcode_Q1310/leetcode_Q1310.cpp
@@ -0,0 +1,39 @@
+﻿// leetcode_Q1310.cpp : このファイルには 'main' 関数が含まれています。プログラム実行の開始と終了がそこで行われます。
+//
+
+#include <iostream>
+#include <vector>
+#include "./../utility/leetcode_testcase_helper.h"
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> xorQueries(vector<int>& arr, vector<vector<int>>& queries) {
+        vector<int> prefix_xors(size(arr) + 1);
+
+        for (size_t i = 0; i < size(arr); i++) {
+            prefix_xors[i + 1] = prefix_xors[i] ^ arr[i];
+        }
+
+        vector<int> ans(size(queries));
+
+        for (size_t i = 0; i < size(queries); i++) {
+            const auto l = queries[i][0];
+            const auto r = queries[i][1];
+            ans[i] = prefix_xors[r + 1] ^ prefix_xors[l];
+        }
+        return ans;
+    }
+};
+
+void test(vector<int>&& arr, vector<vector<int>>&& queries) {
+    output(Solution().xorQueries(arr, queries));
+}
+
+int main()
+{
+    test(get_list_int("[1,3,4,8]"), get_matrix_int("[[0,1],[1,2],[0,3],[3,3]]"));
+    test(get_list_int("[4,8,2,10]"), get_matrix_int("[[2,3],[1,3],[0,0],[0,3]]"));
+    return 0;
+}
---
+++ leetcode_Q1684/leetcode_Q1684.cpp
@@ -0,0 +1,32 @@
+﻿// leetcode_Q1684.cpp : このファイルには 'main' 関数が含まれています。プログラム実行の開始と終了がそこで行われます。
+//
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <unordered_set>
+#include <algorithm>
+#include "./../utility/leetcode_testcase_helper.h"
+
+using namespace std;
+
+class Solution {
+public:
+    int countConsistentStrings(string allowed, vector<string>& words) {
+        unordered_set exists(cbegin(allowed), cend(allowed));
+        const auto count = count_if(cbegin(words), cend(words), [&](const auto& s) { return all_of(cbegin(s), cend(s), [&](const auto& c) { return exists.count(c) > 0; }); });
+        return static_cast<int>(count);
+    }
+};
+
+void test(string allowed, vector<string>&& words) {
+    cout << Solution().countConsistentStrings(allowed, words) << endl;
+}
+
+int main()
+{
+    test("ab", get_list_str(R"(["ad","bd","aaab","baa","badab"])"));
+    test("abc", get_list_str(R"(["a","b","c","ab","ac","bc","abc"])"));
+    test("cad", get_list_str(R"(["cc","acd","b","ba","bac","bad","ac","d"])"));
+    return 0;
+}
