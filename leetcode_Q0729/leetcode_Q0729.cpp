6ce7827cb28ce470d9ece3651681286ffb3ec8bf
Kazuho Sakaoka <kazuho-sakaoka@outlook.jp>
2024-09-26 10:23:30 +0900

    Q729

---
+++ leetcode_Q0729/leetcode_Q0729.cpp
@@ -0,0 +1,50 @@
+﻿// leetcode_Q0729.cpp : このファイルには 'main' 関数が含まれています。プログラム実行の開始と終了がそこで行われます。
+//
+
+#include <iostream>
+#include <map>
+#include <vector>
+
+using namespace std;
+
+class MyCalendar {
+public:
+    MyCalendar() {
+    }
+
+    bool book(int start, int end) {
+        if (!schedule_map_.empty()) {
+            const auto it = schedule_map_.lower_bound(start);
+            if (it != cend(schedule_map_)) {
+                if (it->first < end) {
+                    return false;
+                }
+            }
+            if (it != cbegin(schedule_map_)) {
+                const auto prev_it = prev(it);
+                if (start < prev_it->second) {
+                    return false;
+                }
+            }
+        }
+        schedule_map_.insert({ start, end });
+        return true;
+    }
+private:
+    map<int, int> schedule_map_;
+};
+
+void test(vector<pair<int,int>>&& args) {
+    MyCalendar target;
+    for (auto&& [start, end] : args) {
+        cout << target.book(start, end) << endl;
+    }
+}
+
+
+int main()
+{
+    test({ {10,20},{10,11},{9, 11},{19,20}, });
+    test({ {10,20},{15,25},{20,30}, });
+    return 0;
+}
