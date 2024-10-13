fe298aaea2c2fe220729c3f085dd2e61d04f42ab
Kazuho Sakaoka <kazuho-sakaoka@outlook.jp>
2024-10-13 13:27:21 +0900

    contest 2/4 .....

---
+++ weekly_contest_418/weekly_contest_418.cpp
@@ -0,0 +1,237 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+#include <stack>
+#include <queue>
+#include <algorithm>
+#include <numeric>
+#include <cmath>
+#include <climits>
+#include <tuple>
+#include <functional>
+#include <cassert>
+#include "./../utility/leetcode_testcase_helper.h"
+
+using namespace std;
+
+namespace problem1 {
+class Solution {
+public:
+    static string itobs(int num) {
+        string v;
+        bool exist = false;
+        for (int i = 31; i >= 0; i--) {
+            if ((num & (1 << i)) != 0) {
+                v += '1';
+                exist = true;
+            }
+            else {
+                if (exist) {
+                    v += '0';
+                }
+            }
+        }
+        return v;
+    }
+
+    int maxGoodNumber(vector<int>& nums) {
+        vector<string> bins;
+        bins.reserve(size(nums));
+        transform(cbegin(nums), cend(nums), back_inserter(bins), itobs);
+        int value = 0;
+        value = max(value, stoi(bins[0] + bins[1] + bins[2], nullptr, 2));
+        value = max(value, stoi(bins[0] + bins[2] + bins[1], nullptr, 2));
+        value = max(value, stoi(bins[1] + bins[0] + bins[2], nullptr, 2));
+        value = max(value, stoi(bins[1] + bins[2] + bins[0], nullptr, 2));
+        value = max(value, stoi(bins[2] + bins[1] + bins[0], nullptr, 2));
+        value = max(value, stoi(bins[2] + bins[0] + bins[1], nullptr, 2));
+        return value;
+    }
+};
+void test(vector<int>&& nums) {
+    cout << Solution().maxGoodNumber(nums) << endl;
+}
+void run() {
+    test(get_list_int("[1,2,3]"));
+    test(get_list_int("[2,8,16]"));
+}
+}
+namespace problem2 {
+class Solution {
+public:
+
+
+    unordered_set<int> bfs(const vector<vector<int>>& graph, int start) {
+        unordered_set<int> arriveds;
+        queue<int> q;
+        q.push(start);
+        arriveds.insert(start);
+        while (!q.empty()) {
+            const auto current = q.front();
+            q.pop();
+            for (auto&& next : graph[current]) {
+                if (arriveds.count(next) == 0) {
+                    arriveds.insert(next);
+                    q.push(next);
+                }
+            }
+        }
+        return arriveds;
+    }
+
+    vector<int> remainingMethods(int n, int k, vector<vector<int>>& invocations) {
+        vector<vector<int>> graph(n);
+        vector<vector<int>> graph2(n);
+        for (auto&& invocate : invocations) {
+            graph[invocate[0]].emplace_back(invocate[1]);
+            graph2[invocate[0]].emplace_back(invocate[1]);
+            graph2[invocate[1]].emplace_back(invocate[0]);
+        }
+        unordered_set<int> suspiciouss = bfs(graph, k);
+        if (size(suspiciouss) == n) {
+            return vector<int>();
+        }
+
+        unordered_set<int> remain;
+
+        int firstsafe = -1;
+        queue<int> q;
+
+
+        for (int i = 0; i < n; i++) {
+            if (suspiciouss.count(i) == 0) {
+                q.push(i);
+                remain.insert(i);
+            }
+        }
+        while (!q.empty()) {
+            const auto current = q.front();
+            q.pop();
+            for (auto&& next : graph2[current]) {
+                if (remain.count(next)==0) {
+                    remain.insert(next);
+                    q.push(next);
+                }
+            }
+        }
+  
+        return vector<int>(cbegin(remain), cend(remain));
+    }
+};
+void test(int n, int k, vector<vector<int>>&& invocations) {
+    output(Solution().remainingMethods(n, k, invocations));
+}
+void run() {
+    test(3, 2, get_matrix_int("[[1,0],[2,0]]"));
+
+    test(4, 1, get_matrix_int("[[1,2],[0,1],[3,2]]"));
+    test(5, 0, get_matrix_int("[[1,2],[0,2],[0,1],[3,4]]"));
+    test(3, 2, get_matrix_int("[[1,2],[0,1],[2,0]]"));
+}
+}
+namespace problem3 {
+class Solution {
+public:
+    vector<vector<int>> constructGridLayout(int n, vector<vector<int>>& edges) {
+        vector<vector<int>> graph(n);
+        for (auto&& edge : edges) {
+            graph[edge[0]].emplace_back(edge[1]);
+            graph[edge[1]].emplace_back(edge[0]);
+        }
+
+
+        vector<size_t> connecting_edges(n);
+        map<size_t, vector<int>> rev_map;
+        for (size_t i = 0; i < n; i++) {
+            connecting_edges[i] = size(graph[i]);
+            rev_map[size(graph[i])].emplace_back(i);
+        }
+        if (size(rev_map[1]) == 2) {
+            //一列
+            vector<vector<int>> ans;
+            vector<int> inner;
+            const int first = rev_map[1].front();
+            const int last = rev_map[1].back();
+            inner.emplace_back(first);
+
+            auto find_next = [&](int current, int before) {
+                for (auto&& next : graph[current]) {
+                    if (next != before) {
+                        return next;
+                    }
+                }
+                return -1;
+                };
+            int before = first;
+            int cur = find_next(first, -1);
+            inner.emplace_back(cur);
+            while (cur != last) {
+                int temp = find_next(cur, before);
+                before = cur;
+                cur = temp;
+                inner.emplace_back(cur);
+            }
+            ans.emplace_back(inner);
+            return ans;
+        }
+        else {
+            const auto& corners = rev_map[2];
+            assert(size(corners) == 4);
+            const auto& edges = rev_map[3];
+            const auto& inners = rev_map[4];
+            const auto edge_counts = size(edges);
+            size_t w = 0, h = 0;
+            for (size_t i = 0; i < edge_counts / 2; i++) {
+                w = 2 + i;
+                h = 2 + (edge_counts / 2 - i);
+                if (w * h == n) {
+                    break;
+                }
+            }
+            assert(w != 0);
+            vector<vector<int>> ans(h, vector<int>(w, 0));
+
+            for (int i = 0; i < h; i++) {
+                for (int j = 0; j < w; j++) {
+
+                }
+            }
+        }
+    }
+};
+
+void test(int n, vector<vector<int>>&& edges) {
+    const auto ans = Solution().constructGridLayout(n, edges);
+    for (auto&& l : ans) {
+        for (auto&& value : l) {
+            cout << value << ",";
+        }
+        cout << endl;
+    }
+    cout << endl;
+}
+void run() {
+    test(5, get_matrix_int("[[0,1],[1,3],[2,3],[2,4]]"));
+    //test(4, get_matrix_int("[[0,1],[0,2],[1,3],[2,3]]"));
+    //test(9, get_matrix_int("[[0,1],[0,4],[0,5],[1,7],[2,3],[2,4],[2,5],[3,6],[4,6],[4,7],[6,8],[7,8]]"));
+}
+}
+
+namespace problem4 {
+void run() {
+
+}
+}
+
+int main()
+{
+    //problem1::run();
+    //problem2::run();
+    problem3::run();
+    problem4::run();
+    return 0;
+}
---
+++ weekly_contest_419/weekly_contest_419.cpp
@@ -0,0 +1,237 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+#include <stack>
+#include <queue>
+#include <algorithm>
+#include <numeric>
+#include <cmath>
+#include <climits>
+#include <tuple>
+#include <functional>
+#include <cassert>
+#include "./../utility/leetcode_testcase_helper.h"
+#include "./../utility/TreeNodeUtil.h"
+
+
+using namespace std;
+
+namespace problem1 {
+class Solution {
+public:
+
+    static int get_x(const map<size_t, set<int>>& rev_map, int x) {
+        auto outer_it = crbegin(rev_map);
+        auto inner_it = crbegin(outer_it->second);
+
+        int total = 0;
+        for (int i = 0; i < x; i++) {
+            const auto& current_set = outer_it->second;
+            if (inner_it == crend(current_set)) {
+                ++outer_it;
+                if (outer_it == crend(rev_map)) {
+                    break;
+                }
+                inner_it = crbegin(outer_it->second);
+            }
+            total += outer_it->first * (*inner_it);
+            ++inner_it;
+        }
+        return total;
+    }
+
+    vector<int> findXSum(vector<int>& nums, int k, int x) {
+
+        vector<int> ans;
+        for (int i = 0; i <= size(nums) - k; i++) {
+            unordered_map<int, size_t> freq_map;
+            for (int j = 0; j < k; j++) {
+                freq_map[nums[j + i]]++;
+            }
+            map<size_t, set<int>> rev_map;
+
+            for (auto&& [num, freq] : freq_map) {
+                rev_map[freq].insert(num);
+            }
+
+            const auto v = get_x(rev_map, x);
+            ans.emplace_back(v);
+        }
+
+        return ans;
+    }
+};
+
+void test(vector<int>&& nums, int k, int x) {
+    output(Solution().findXSum(nums, k, x));
+}
+void run() {
+    test(get_list_int("[9,2,2]"), 3, 3);
+    test(get_list_int("[1,1,2,2,3,4,2,3]"), 6, 2);
+    test(get_list_int("[3,8,7,8,7,5]"), 2, 2);
+}
+}
+namespace problem2 {
+class Solution {
+public:
+
+    static pair<int,int> rec(const TreeNode* node, map<int, int>& pbt_size_map) {
+        if (node == nullptr) {
+            return {0,0};
+        }
+
+        const auto [left_depth, nodes1] = rec(node->left, pbt_size_map);
+        const auto [right_depth, nodes2] = rec(node->right, pbt_size_map);
+        if (left_depth == right_depth && left_depth >= 0) {
+            int pbt_size = 1 + nodes1 + nodes2;
+            pbt_size_map[pbt_size]++;
+            return { left_depth + 1, pbt_size };
+        }
+        else {
+            return { -1,-1 };
+        }
+    }
+
+    int kthLargestPerfectSubtree(TreeNode* root, int k) {
+        map<int, int> pbt_size_map;
+        rec(root, pbt_size_map);
+        vector<int> rev;
+        for (auto&& [s, freq] : pbt_size_map) {
+            for (int i = 0; i < freq; i++) {
+                rev.emplace_back(s);
+            }
+        }
+        if (size(rev) < k) {
+            return -1;
+        }
+        sort(begin(rev), end(rev), std::greater<int>());
+        auto it = cbegin(rev);
+        for (int i = 0; i < k - 1; i++) {
+            ++it;
+        }
+        return *it;
+    }
+};
+
+void test(vector<optional<int>>&& values, int k) {
+    TreeNode* root = create_treenode2(values);
+    cout << Solution().kthLargestPerfectSubtree(root, k) << endl;
+}
+
+void run() {
+    test(get_list_optional_int("[5,3,6,5,2,5,7,1,8,null,null,6,8]"), 2);
+    test(get_list_optional_int("[1,2,3,4,5,6,7]"), 1);
+    test(get_list_optional_int("[1,2,3,null,4]"), 3);
+    test(get_list_optional_int("[1,2,3,null,4]"), 2);
+}
+}
+namespace problem3 {
+constexpr long long MODULO = 1e9 + 7;
+class Solution {
+public:
+
+    static size_t get_index(char c) {
+        switch (c) {
+        case 'F':
+            return 0;
+        case 'W':
+            return 1;
+        case 'E':
+            return 2;
+        }
+        throw exception();
+    }
+
+    static void calc_pattern(map<int, long long>& inner_map, int transit_val, map<int, long long>& before_map) {
+        for (auto&& [diff, counts] : before_map) {
+            inner_map[diff + transit_val] += counts;
+            inner_map[diff + transit_val] %= MODULO;
+        }
+    }
+
+    static int get_transit_val(char me, char other) {
+        if (me == other) { return 0; }
+        switch (me) {
+        case 'F':
+            return other == 'W' ? -1 : 1;
+        case 'W':
+            return other == 'E' ? -1 : 1;
+        case 'E':
+            return other == 'F' ? -1 : 1;
+        }
+        throw exception();
+    }
+
+    int countWinningSequences(string s) {
+        vector<vector<map<int, long long>>> dp(size(s), vector<map<int, long long>>(3));
+
+        switch (s[0]) {
+        case 'F':
+            dp[0][get_index('F')] = map<int, long long>{ {0,1} };
+            dp[0][get_index('W')] = map<int, long long>{ {1,1} };
+            dp[0][get_index('E')] = map<int, long long>{ {-1,1} };
+            break;
+        case 'W':
+            dp[0][get_index('F')] = map<int, long long>{ {-1,1} };
+            dp[0][get_index('W')] = map<int, long long>{ {0,1} };
+            dp[0][get_index('E')] = map<int, long long>{ {1,1} };
+            break;
+        case 'E':
+            dp[0][get_index('F')] = map<int, long long>{ {1,1} };
+            dp[0][get_index('W')] = map<int, long long>{ {-1,1} };
+            dp[0][get_index('E')] = map<int, long long>{ {0,1} };
+            break;
+        }
+
+        for (size_t i = 1; i < size(s); i++) {
+            calc_pattern(dp[i][get_index('F')], get_transit_val('F', s[i]), dp[i - 1][get_index('W')]);
+            calc_pattern(dp[i][get_index('F')], get_transit_val('F', s[i]), dp[i - 1][get_index('E')]);
+
+            calc_pattern(dp[i][get_index('W')], get_transit_val('W', s[i]), dp[i - 1][get_index('F')]);
+            calc_pattern(dp[i][get_index('W')], get_transit_val('W', s[i]), dp[i - 1][get_index('E')]);
+
+            calc_pattern(dp[i][get_index('E')], get_transit_val('E', s[i]), dp[i - 1][get_index('F')]);
+            calc_pattern(dp[i][get_index('E')], get_transit_val('E', s[i]), dp[i - 1][get_index('W')]);
+        }
+
+        long long total = 0;
+        for (auto&& inner_map : dp[size(s) - 1]) {
+            for (auto&& [score, count] : inner_map) {
+                if (0 < score) {
+                    total += count;
+                    total %= MODULO;
+                }
+            }
+        }
+
+        return static_cast<int>(total % MODULO);
+    }
+};
+
+void test(string&& s) {
+    cout << Solution().countWinningSequences(s) << endl;
+}
+void run() {
+    test("FFF");
+    test("FWEFW");
+}
+}
+
+namespace problem4 {
+    void run() {
+
+    }
+}
+
+int main()
+{
+    //problem1::run();
+    //problem2::run();
+    problem3::run();
+    problem4::run();
+    return 0;
+}
