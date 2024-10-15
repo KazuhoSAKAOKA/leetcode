affe50003f50512369afb5af8a8d2e8e4fb3d3d3
Kazuho Sakaoka <kazuho-sakaoka@outlook.jp>
2024-10-15 20:03:38 +0900

    Q2938

---
+++ leetcode_Q2938/leetcode_Q2938.cpp
@@ -0,0 +1,37 @@
+﻿// leetcode_Q2938.cpp : このファイルには 'main' 関数が含まれています。プログラム実行の開始と終了がそこで行われます。
+//
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+    long long minimumSteps(string s) {
+        long long count = 0;
+        long long current_one_count = 0;
+
+        for (auto&& c : s) {
+            if (c == '1') {
+                current_one_count++;
+            }
+            else {
+                count += current_one_count;
+            }
+        }
+        return count;
+    }
+};
+
+void test(string&& s) {
+    cout << Solution().minimumSteps(s) << endl;
+}
+
+int main()
+{
+    test("101");
+    test("100");
+    test("0111");
+    return 0;
+}
