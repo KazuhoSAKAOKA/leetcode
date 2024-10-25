d732c4dc5fab440e0a3a9d22f90d2aef2dc20577
Kazuho Sakaoka <kazuho-sakaoka@outlook.jp>
2024-10-25 23:06:21 +0900

    Q1233 help

---
+++ leetcode_Q0951/leetcode_Q0951.cpp
@@ -0,0 +1,45 @@
+﻿// leetcode_Q0951.cpp : このファイルには 'main' 関数が含まれています。プログラム実行の開始と終了がそこで行われます。
+//
+
+#include <iostream>
+#include <vector>
+#include <optional>
+
+#include "./../utility/leetcode_testcase_helper.h"
+#include "./../utility/TreeNodeUtil.h"
+
+using namespace std;
+
+class Solution {
+public:
+
+    static bool dfs(const TreeNode* node1, const TreeNode* node2) {
+        if (node1 == nullptr || node2 == nullptr) {
+            return node1 == node2;
+        }
+        if (node1->val != node2->val) {
+            return false;
+        }
+        if (dfs(node1->left, node2->right) && dfs(node1->right, node2->left)) {
+            return true;
+        }
+        if (dfs(node1->left, node2->left) && dfs(node1->right, node2->right)) {
+            return true;
+        }
+        return false;
+    }
+    bool flipEquiv(TreeNode* root1, TreeNode* root2) {
+        return dfs(root1, root2);
+    }
+};
+
+void test(vector<optional<int>>&& values1, vector<optional<int>>&& values2) {
+    cout << Solution().flipEquiv(create_treenode2(values1), create_treenode2(values2)) << endl;
+}
+int main()
+{
+    test(get_list_optional_int("[1,2,3,4,5,6,null,null,null,7,8]"), get_list_optional_int("[1,3,2,null,6,4,5,null,null,null,null,8,7]"));
+    test(get_list_optional_int("[]"), get_list_optional_int("[]"));
+    test(get_list_optional_int("[]"), get_list_optional_int("[1]"));
+    return 0;
+}
---
+++ leetcode_Q1233/leetcode_Q1233.cpp
@@ -0,0 +1,67 @@
+﻿// leetcode_Q1233.cpp : このファイルには 'main' 関数が含まれています。プログラム実行の開始と終了がそこで行われます。
+//
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+#include "./../utility/leetcode_testcase_helper.h"
+using namespace std;
+
+class Solution {
+public:
+
+    static bool is_subfolder(const string& current, const string& target) {
+        if (size(target) < size(current)) {
+            return false;
+        }
+        const auto temp = target.substr(0, size(current));
+        if (temp != current) {
+            return false;
+        }
+        if (size(target) == size(current)) {
+            throw exception();
+        }
+        if (target[size(current)] == '/') {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    vector<string> removeSubfolders(vector<string>& folder) {
+        sort(begin(folder), end(folder));
+
+
+        vector<string> ans;
+
+        int index = 1;
+        string current = folder.front();
+        ans.emplace_back(current);
+        while (index < size(folder)) {
+            if (!is_subfolder(current, folder[index])) {
+                current = folder[index];
+                ans.emplace_back(folder[index]);
+            }
+            index++;
+        }
+
+
+        return ans;
+
+    }
+};
+
+void test(vector<string>&& folder) {
+    output(Solution().removeSubfolders(folder));
+}
+
+int main()
+{
+    test(get_list_str(R"(["/a","/a/b","/c/d","/c/d/e","/c/f"])"));
+    test(get_list_str(R"(["/a","/a/b/c","/a/b/d"])"));
+    test(get_list_str(R"(["/a/b/c","/a/b/ca","/a/b/d"])"));
+    return 0;
+}
