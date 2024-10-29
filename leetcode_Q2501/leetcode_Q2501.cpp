5c810a8f64d46399e10d44b1aa4b59a366921047
Kazuho Sakaoka <kazuho-sakaoka@outlook.jp>
2024-10-29 20:09:38 +0900

    Q2501 2684

---
+++ leetcode_Q2501/leetcode_Q2501.cpp
@@ -0,0 +1,44 @@
+﻿// leetcode_Q2501.cpp : このファイルには 'main' 関数が含まれています。プログラム実行の開始と終了がそこで行われます。
+//
+
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <algorithm>
+
+#include "./../utility/leetcode_testcase_helper.h"
+
+using namespace std;
+
+class Solution {
+public:
+    int longestSquareStreak(vector<int>& nums) {
+        sort(begin(nums), end(nums));
+        unordered_map <long long, int> prev_map;
+        int max_streak = -1;
+        for(auto&& num : nums){
+            const long long n2 = static_cast<long long>(num) * static_cast<long long>(num);
+            auto it = prev_map.find(num);
+            if (it != end(prev_map)) {
+                const auto streak = it->second + 1;
+                max_streak = max(max_streak, streak);
+                prev_map[n2] = max(prev_map[n2], streak);
+            }
+            else {
+                prev_map[n2] = 1;
+            }
+        }
+        return max_streak;
+    }
+};
+
+void test(vector<int>&& nums) {
+    cout << Solution().longestSquareStreak(nums) << endl;
+}
+
+int main()
+{
+    test(get_list_int("[4,3,6,16,8,2]"));
+    test(get_list_int("[2,3,5,6,7]"));
+    return 0;
+}
---
+++ leetcode_Q2684/leetcode_Q2684.cpp
@@ -0,0 +1,48 @@
+﻿// leetcode_Q2684.cpp : このファイルには 'main' 関数が含まれています。プログラム実行の開始と終了がそこで行われます。
+//
+
+#include <iostream>
+#include <vector>
+
+#include "./../utility/leetcode_testcase_helper.h"
+
+using namespace std;
+
+class Solution {
+public:
+    int maxMoves(vector<vector<int>>& grid) {
+        const auto W = size(grid.front());
+        const auto H = size(grid);
+        vector<vector<int>> dp(W, vector<int>(H, -1));
+        const vector<int> movables{ -1,0,1 };
+        int max_moves = 0;
+        for (int j = 0; j < H; j++) {
+            dp[0][j] = 0;
+        }
+
+        for (int i = 1; i < W; i++) {
+            for (int j = 0; j < H; j++) {
+                for (int moveval : movables) {
+                    const auto v = j + moveval;
+                    if (0 <= v && v < H && 0 <= dp[i - 1][v]) {
+                        if (grid[v][i - 1] < grid[j][i]) {
+                            dp[i][j] = max(dp[i][j], dp[i - 1][v] + 1);
+                        }
+                    }
+                    max_moves = max(max_moves, dp[i][j]);
+                }
+            }
+        }
+        return max_moves;
+    }
+};
+
+void test(vector<vector<int>>&& grid) {
+    cout << Solution().maxMoves(grid) << endl;
+}
+int main()
+{
+    test(get_matrix_int("[[3,2,4],[2,1,9],[1,1,7]]"));
+    test(get_matrix_int("[[2,4,3,5],[5,4,9,3],[3,4,2,11],[10,9,13,15]]"));
+    return 0;
+}
