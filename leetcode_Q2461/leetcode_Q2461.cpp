65e708a90effea17f2e61edf00eb64fc1bf4b1f1
Kazuho Sakaoka <kazuho-sakaoka@outlook.jp>
2024-11-20 21:12:34 +0900

    Q2516,2461

---
+++ leetcode_Q2461/leetcode_Q2461.cpp
@@ -0,0 +1,53 @@
+﻿// leetcode_Q2461.cpp : このファイルには 'main' 関数が含まれています。プログラム実行の開始と終了がそこで行われます。
+//
+
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+
+#include "./../utility/leetcode_testcase_helper.h"
+
+using namespace std;
+
+class Solution {
+public:
+    long long maximumSubarraySum(vector<int>& nums, int k) {
+        long long max_sum = 0;
+        unordered_map<int, int> freq_map;
+        long long current_sum = 0;
+        for (int i = 0; i < k; i++) {
+            freq_map[nums[i]]++;
+            current_sum += static_cast<long long>(nums[i]);
+        }
+
+        for (int i = k; i < size(nums); i++) {
+            if (size(freq_map) == k) {
+                max_sum = max(max_sum, current_sum);
+            }
+            freq_map[nums[i]]++;
+            current_sum += static_cast<long long>(nums[i]);
+
+            freq_map[nums[i - k]]--;
+            current_sum -= static_cast<long long>(nums[i - k]);
+            if (freq_map[nums[i - k]] == 0) {
+                freq_map.erase(nums[i - k]);
+            }
+        }
+        if (size(freq_map) == k) {
+            max_sum = max(max_sum, current_sum);
+        }
+        return max_sum;
+    }
+};
+
+void test(vector<int>&& nums, int k) {
+    cout << Solution().maximumSubarraySum(nums, k) << endl;
+}
+
+int main()
+{
+    test(get_list_int("[1,1,1,7,8,9]"), 3);
+    test(get_list_int("[1,5,4,2,9,9,9]"), 3);
+    test(get_list_int("[4,4,4]"), 3);
+    return 0;
+}
---
+++ leetcode_Q2516/leetcode_Q2516.cpp
@@ -0,0 +1,52 @@
+﻿// leetcode_Q2516.cpp : このファイルには 'main' 関数が含まれています。プログラム実行の開始と終了がそこで行われます。
+//
+
+#include <iostream>
+#include <vector>
+
+#include "./../utility/leetcode_testcase_helper.h"
+
+using namespace std;
+
+class Solution {
+public:
+    int takeCharacters(string s, int k) {
+        vector<int> freqs(3, 0);
+        for (auto&& c : s) {
+            freqs[c - 'a']++;
+        }
+        const int limita = freqs[0] - k;
+        const int limitb = freqs[1] - k;
+        const int limitc = freqs[2] - k;
+        if (limita < 0 || limitb < 0 || limitc < 0) {
+            return -1;
+        }
+        if (k == 0) {
+            return 0;
+        }
+        int min_takes = size(s);
+        vector<int> current_state(3, 0);
+        int l = 0;
+        for (int r = 0; r < size(s); r++) {
+            current_state[s[r] - 'a']++;
+            while (l < r && (limita < current_state[0] || limitb < current_state[1] || limitc < current_state[2])) {
+                current_state[s[l] - 'a']--;
+                l++;
+            }
+            if (current_state[0] <= limita && current_state[1] <= limitb && current_state[2]<= limitc) {
+                min_takes = min(min_takes, static_cast<int>(size(s) - (r - l + 1)));
+            }
+        }
+        return min_takes;
+    }
+};
+
+void test(string&& s, int k) {
+    cout << Solution().takeCharacters(s, k) << endl;
+}
+int main()
+{
+    test("aabaaaacaabc", 2);
+    test("a", 1);
+    return 0;
+}
