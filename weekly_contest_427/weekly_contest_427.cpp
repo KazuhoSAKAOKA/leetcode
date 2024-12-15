1ed7a4bdb8b244cf87cd70d78a68ff53d9a36df4
Kazuho Sakaoka <kazuho-sakaoka@outlook.jp>
2024-12-15 17:05:36 +0900

    weekly contests 2/4...

---
+++ weekly_contest_427/weekly_contest_427.cpp
@@ -0,0 +1,204 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+#include <stack>
+#include <queue>
+#include <algorithm>
+#include <numeric>
+#include <cmath>
+#include <climits>
+#include <tuple>
+#include <functional>
+#include "./../utility/leetcode_testcase_helper.h"
+
+using namespace std;
+
+namespace problem1 {
+class Solution {
+public:
+    vector<int> constructTransformedArray(vector<int>& nums) {
+        const auto N = size(nums);
+        vector<int> ans(N);
+
+        for (int i = 0; i < N; i++) {
+            if (nums[i] > 0) {
+                ans[i] = nums[(i + nums[i]) % N];
+            }
+            else if (nums[i] < 0) {
+                const auto n = -1 * static_cast<int>((abs(nums[i]) % N));
+                ans[i] = nums[(N + i + n) % N];
+            }
+            else {
+                ans[i] = nums[i];
+            }
+        }
+        return ans;
+    }
+};
+static void test(vector<int>&& nums) {
+    output(Solution().constructTransformedArray(nums));
+}
+static void run() {
+    test(get_list_int("[-1,4,-1]"));
+    test(get_list_int("[-10,-10,-4]"));
+    test(get_list_int("[3,-2,1,1]"));
+    test(get_list_int("[-1,4,-1]"));
+}
+}
+
+
+namespace problem2 {
+class Solution {
+public:
+/*
+    static         int maxRectangleArea1(vector<vector<int>>&points) {
+            map<int, map<int, size_t>> x_ref_map;
+            for (size_t index = 0; index < size(points); index++) {
+                const auto point = points[index];
+                const auto x = point.front();
+                const auto y = point.back();
+                x_ref_map[x][y] = index;
+            }
+            int max_area = -1;
+
+            for (size_t index = 0; index < size(points); index++) {
+                const auto point = points[index];
+                const auto x = point.front();
+                const auto y = point.back();
+
+                auto next_x_it = x_ref_map.upper_bound(x);
+                while (next_x_it != cend(x_ref_map)) {
+                    if () { continue; }
+                    auto& x_ref_map_target = x_ref_map[x];
+                    const auto next_y_it = x_ref_map_target.upper_bound(y);
+                    if (next_y_it == cend(x_ref_map_target)) { continue; }
+                    const auto right = next_x_it->first;
+                    const auto bottom = next_y_it->first;
+
+                    const auto right_x_it = x_ref_map.find(right);
+                    if (right_x_it == cend(x_ref_map)) { continue; }
+                    const auto& right_bottom_map = right_x_it->second;
+                    const auto bottom_y_it = right_bottom_map.find(bottom);
+                    if (bottom_y_it == cend(right_bottom_map)) { continue; }
+                    max_area = max(max_area, (right - x) * (bottom - y));
+
+
+                    ++next_x_it;
+                }
+            }
+    }
+    */
+    int maxRectangleArea(vector<vector<int>>& points) {
+        map<int, set<size_t>> x_ref_map;
+        map<int, set<size_t>> y_ref_map;
+        for (size_t index = 0; index < size(points); index++) {
+            const auto point = points[index];
+            const auto x = point.front();
+            const auto y = point.back();
+            x_ref_map[x].insert(index);
+            y_ref_map[y].insert(index);
+        }
+        int max_area = -1;
+        for (int i = 0; i < size(points); i++) {
+            const auto point = points[i];
+            const auto x = point.front();
+            const auto y = point.back();
+            for (auto&& index_x : x_ref_map[x]) {
+                if (index_x == i) { continue; }
+                const auto other_y = points[index_x].back();
+     
+                const auto& y_ref_map_cur = y_ref_map[y];
+                const auto& y_ref_map_other = y_ref_map[other_y];
+                if (size(y_ref_map_cur) > 1 && size(y_ref_map_other) > 1) {
+                    for (auto&& y_index1 : y_ref_map_cur) {
+                        if (y_index1 == i) { continue; }
+                        for (auto&& y_index2 : y_ref_map_other) {
+                            if (y_index2 == index_x) { continue; }
+                            if (points[y_index2].front() == points[y_index1].front()) {
+                                const auto other_x = points[y_index2].front();
+                                const int left = min(x, other_x);
+                                const int right = max(x, other_x);
+                                const int bottom = min(y, other_y);
+                                const int top = max(y, other_y);
+                                bool contains = false;
+                                for (int check_index = 0; check_index < size(points); check_index++) {
+                                    if (check_index == i || check_index == index_x || check_index == y_index1 || check_index == y_index2) {
+                                        continue;
+                                    }
+                                    const auto check_x = points[check_index].front();
+                                    const auto check_y = points[check_index].back();
+                                    if (
+                                        (left <= check_x && check_x <= right) && (bottom <= check_y && check_y <= top)
+                                        ) {
+                                        contains = true;
+                                        break;
+                                    }
+                                }
+                                if (!contains) {
+                                    max_area = max(max_area, (right - left) * (top - bottom));
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        return max_area;
+    }
+};
+void test(vector<vector<int>>&& points) {
+    cout << Solution().maxRectangleArea(points) << endl;
+}
+static void run() {
+    test(get_matrix_int("[[96,44],[23,72],[96,72],[23,44],[65,44]]"));
+    test(get_matrix_int("[[1,1],[1,3],[3,1],[3,3]]"));
+    test(get_matrix_int("[[100,80],[67,79],[100,79],[67,80],[80,47]]"));
+    test(get_matrix_int("[[1,1],[1,3],[3,1],[3,3],[2,2]]"));
+    test(get_matrix_int("[[1,1],[1,3],[3,1],[3,3],[1,2],[3,2]]"));
+}
+}
+namespace problem3 {
+class Solution {
+public:
+    long long maxSubarraySum(vector<int>& nums, int k) {
+        vector<long long> prefix_sums(size(nums) + 1);
+        for (int i = 0; i < size(nums); i++) {
+            prefix_sums[i + 1] = prefix_sums[i] + static_cast<long long>(nums[i]);
+        }
+        long long max_sum = LLONG_MIN;
+        for (int i = 0; i < size(nums); i++) {
+            for (int j = i + k; j <= size(nums); j += k) {
+                max_sum = max(max_sum, prefix_sums[j] - prefix_sums[i]);
+            }
+        }
+        return max_sum;
+    }
+}; 
+static void test(vector<int>&& nums, int k) {
+    cout << Solution().maxSubarraySum(nums, k) << endl;
+}
+static void run() {
+    test(get_list_int("[8]"), 1);
+    test(get_list_int("[1,2]"), 1);
+    test(get_list_int("[-1,-2,-3,-4,-5]"), 4);
+    test(get_list_int("[-5,1,2,-3,4]"), 2);
+}
+}
+namespace problem4 {
+static void run() {
+}
+}
+
+int main()
+{
+    //problem1::run();
+    //problem2::run();
+    problem3::run();
+    problem4::run();
+    return 0;
+}
---
+++ weekly_contest_428/weekly_contest_428.cpp
@@ -0,0 +1,224 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+#include <stack>
+#include <queue>
+#include <algorithm>
+#include <numeric>
+#include <cmath>
+#include <climits>
+#include <tuple>
+#include <functional>
+#include "./../utility/leetcode_testcase_helper.h"
+
+using namespace std;
+
+namespace problem1 {
+class Solution {
+public:
+    int buttonWithLongestTime(vector<vector<int>>& events) {
+        map<int, int> duration_map;
+        int before_time = 0;
+        for (int i = 0; i < size(events); i++) {
+            const auto& event = events[i];
+            const auto button = event.front();
+            const auto time = event.back();
+            const auto duration = time - before_time;
+            duration_map[button] = max(duration_map[button],duration);
+            before_time = time;
+        }
+        int max_duration = 0;
+        int longest_button = -1;
+        for (auto&& [button, duration] : duration_map) {
+            if (max_duration < duration) {
+                longest_button = button;
+                max_duration = duration;
+            }
+        }
+        return longest_button;
+    }
+}; 
+void test(vector<vector<int>>&& events) {
+    cout << Solution().buttonWithLongestTime(events) << endl;
+}
+static void run() {
+    test(get_matrix_int("[[7,1],[19,3],[9,4],[12,5],[2,8],[15,10],[18,12],[7,14],[19,16]]"));
+    test(get_matrix_int("[[1,2],[2,5],[3,9],[1,15]]"));
+    test(get_matrix_int("[[10,5],[1,7]]"));
+}
+}
+namespace problem2 {
+class Solution {
+public:
+    using MYLIST_T = vector<pair<vector<vector<string>>, vector<double>>>;
+
+    static double rec(const MYLIST_T& list, int index, const string& initialCurrency, const string& currentCurrency, double current) {
+        if (index == size(list)) {
+            if (initialCurrency == currentCurrency) {
+                return current;
+            }
+            return 0.0;
+        }
+
+        const auto& [curPairs, rates] = list[index];
+
+        queue<pair<string, double>> qu;
+        map<string, double> used_map;
+        used_map[currentCurrency] = current;
+        qu.push({ currentCurrency, current });
+        while (!qu.empty()) {
+            const auto [cur_curr, value] = qu.front();
+            qu.pop();
+            for (int i = 0; i < size(curPairs); i++) {
+                const auto& p = curPairs[i];
+                if (p.front() == cur_curr) {
+                    const auto v = value * rates[i];
+                    if (used_map[p.back()] < v) {
+                        used_map[p.back()] = v;
+                        qu.push({ p.back(), v });
+                    }
+                }
+                if (p.back() == cur_curr) {
+                    const auto v = value * (1 / rates[i]);
+                    if (used_map[p.front()] < v) {
+                        used_map[p.front()] = v;
+                        qu.push({p.front(), v});
+                    }
+                }
+            }
+        }
+
+
+        double max_currency = 0;
+        for (auto&& [cur, value] : used_map) {
+            max_currency = max(max_currency, rec(list, index + 1, initialCurrency, cur, value));
+        }
+        return max_currency;
+    }
+
+    double maxAmount(string initialCurrency, vector<vector<string>>& pairs1, vector<double>& rates1, vector<vector<string>>& pairs2, vector<double>& rates2) {
+        MYLIST_T list{
+            {pairs1,rates1 },
+            {pairs2,rates2 },
+        };
+
+        return rec(list, 0, initialCurrency, initialCurrency, 1.0);
+    }
+};
+static void test(string&& initialCurrency, vector<vector<string>>&& pairs1, vector<double>&& rates1, vector<vector<string>>&& pairs2, vector<double>&& rates2) {
+    cout << Solution().maxAmount(initialCurrency, pairs1, rates1, pairs2, rates2) << endl;
+}
+static void run() {
+    test("EUR", get_matrix_str(R"([["EUR","USD"],["USD","JPY"]])"), get_list_double("[2.0,3.0]"), get_matrix_str(R"([["JPY","USD"],["USD","CHF"],["CHF","EUR"]])"), get_list_double("[4.0,5.0,6.0]"));
+    test("NGN", get_matrix_str(R"([["NGN","EUR"]])"), get_list_double("[9.0]"), get_matrix_str(R"([["NGN","EUR"]])"), get_list_double("[6.0]"));
+    test("USD", get_matrix_str(R"([["USD","EUR"]])"), get_list_double("[1.0]"), get_matrix_str(R"([["EUR","JPY"]])"), get_list_double("[10.0]"));
+}
+}
+namespace problem3 {
+class Solution {
+public:
+
+    static bool check_prefix(const vector<int>& nums, int len, int s1, int s2) {
+        for (int i = 0; i < len; i++) {
+            if (size(nums) <= (s1 + i) || size(nums) <= (s2 + i)) { return false; }
+            if (nums[s1 + i] != nums[s2 + i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    int sol(vector<int>& nums) {
+        int n = nums.size();
+        int count = 0;
+        for (int i = 1; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                bool isValid1 =
+                    (j - i >= i) &&
+                    equal(nums.begin(), nums.begin() + i, nums.begin() + i);
+                bool isValid2 =
+                    (n - j >= j - i) &&
+                    equal(nums.begin() + i, nums.begin() + j, nums.begin() + j);
+                if (isValid1 || isValid2) {
+                    cout << i << "," << j << endl;
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+
+    int beautifulSplits(vector<int>& nums) {
+        const auto N = size(nums);
+        if (N < 3) {
+            return 0;
+        }
+        vector<vector<int>> dp(N + 1, vector<int>(N + 1));
+        for (int len = 1; len <= N; len++) {
+            for (int index = N - len - 1; index >= 0; index--) {
+                if (nums[index] == nums[index + len]) {
+                    dp[len][index] = dp[len][index + 1] + 1;
+                }
+                else {
+                    dp[len][index] = 0;
+                }
+            }
+        }
+        int count = 0;
+
+        for (int num1_len = 1; num1_len <= N - 2; num1_len++) {
+            if (dp[num1_len][0] >= num1_len) {
+                int remain_length = N - 2 * num1_len;
+                int cur_patterns = remain_length;
+                count += cur_patterns;
+                const auto num2_begin = num1_len;
+                for (int num2_len = 1; num2_len < num1_len; num2_len++) {
+                    if (dp[num2_len][num2_begin] >= num2_len) {
+                        count++;
+                    }
+                }
+            }
+            else {
+                const auto num2_begin = num1_len;
+                for (int num2_len = 1; num2_len <= N - num1_len - 1; num2_len++) {
+                    if (dp[num2_len][num2_begin] >= num2_len) {
+                        count++;
+                    }
+                }
+            }
+        }
+        return count;
+    }
+}; 
+
+void test(vector<int>&& nums) {
+    cout << Solution().beautifulSplits(nums) << endl;
+}
+static void run() {
+    test(get_list_int("[0,0,0,0,2,2,0,1,2,1,2]"));
+    //test(get_list_int("[3,3,3,1,3]"));
+    //test(get_list_int("[1,1,2]"));
+    //test(get_list_int("[0]"));
+    //test(get_list_int("[1,1,2,1]"));
+    //test(get_list_int("[2,2,0,0,0,0,0,1,2,2,0,0,0,1,0]"));
+    //test(get_list_int("[1,2,3,4]"));
+}
+}
+namespace problem4 {
+static void run() {
+}
+}
+
+int main()
+{
+    //problem1::run();
+    //problem2::run();
+    problem3::run();
+    problem4::run();
+    return 0;
+}
