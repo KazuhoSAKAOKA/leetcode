9508f29e266a44fe9ac170360d89ed6a67b4249c
Kazuho Sakaoka <kazuho-sakaoka@outlook.jp>
2025-01-13 15:29:08 +0900

    Q3223

---
+++ leetcode_Q3223/leetcode_Q3223.cpp
@@ -0,0 +1,47 @@
+﻿// leetcode_Q3223.cpp : このファイルには 'main' 関数が含まれています。プログラム実行の開始と終了がそこで行われます。
+//
+
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+    int minimumLength(string s) {
+        vector<int> freqs(26);
+        for (auto&& c : s) {
+            freqs[c - 'a']++;
+        }
+
+        int count = 0;
+        for (auto&& freq : freqs) {
+            if (freq <= 2) {
+                count += freq;
+            }
+            else {
+                if (freq % 2 == 1) {
+                    count += 1;
+                }
+                else {
+                    count += 2;
+                }
+            }
+        }
+        return count;
+    }
+};
+
+void test(string&& s) {
+    cout << Solution().minimumLength(s) << endl;
+}
+
+
+
+int main()
+{
+    test("abaacbcbb");
+    test("aa");
+    return 0;
+}
