78413cb734db22e908ccd803c6559f6ff3d14b72
Kazuho Sakaoka <kazuho-sakaoka@outlook.jp>
2025-02-15 13:06:08 +0900

    Q2698

---
+++ leetcode_Q1352/leetcode_Q1352.cpp
@@ -0,0 +1,53 @@
+﻿// leetcode_Q1352.cpp : このファイルには 'main' 関数が含まれています。プログラム実行の開始と終了がそこで行われます。
+//
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+class ProductOfNumbers {
+    vector<long long> prefix_products_;
+    vector<int> prefix_zeros_;
+public:
+    ProductOfNumbers() :prefix_products_(1, 1), prefix_zeros_(1, 0) {
+
+    }
+
+    void add(int num) {
+        if (num == 0) {
+            prefix_products_.emplace_back(1 * prefix_products_.back());
+            prefix_zeros_.emplace_back(1 + prefix_zeros_.back());
+        }
+        else {
+            prefix_products_.emplace_back(num * prefix_products_.back());
+            prefix_zeros_.emplace_back(prefix_zeros_.back());
+        }
+    }
+
+    int getProduct(int k) {
+        const auto index = size(prefix_products_) - k - 1;
+        if (prefix_zeros_.back() == prefix_zeros_[index]) {
+            return static_cast<int>(prefix_products_.back() / prefix_products_[index]);
+        }
+        else {
+            return 0;
+        }
+    }
+};
+
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
+// プログラムの実行: Ctrl + F5 または [デバッグ] > [デバッグなしで開始] メニュー
+// プログラムのデバッグ: F5 または [デバッグ] > [デバッグの開始] メニュー
+
+// 作業を開始するためのヒント: 
+//    1. ソリューション エクスプローラー ウィンドウを使用してファイルを追加/管理します 
+//   2. チーム エクスプローラー ウィンドウを使用してソース管理に接続します
+//   3. 出力ウィンドウを使用して、ビルド出力とその他のメッセージを表示します
+//   4. エラー一覧ウィンドウを使用してエラーを表示します
+//   5. [プロジェクト] > [新しい項目の追加] と移動して新しいコード ファイルを作成するか、[プロジェクト] > [既存の項目の追加] と移動して既存のコード ファイルをプロジェクトに追加します
+//   6. 後ほどこのプロジェクトを再び開く場合、[ファイル] > [開く] > [プロジェクト] と移動して .sln ファイルを選択します
---
+++ leetcode_Q2698/leetcode_Q2698.cpp
@@ -0,0 +1,53 @@
+﻿// leetcode_Q2698.cpp : このファイルには 'main' 関数が含まれています。プログラム実行の開始と終了がそこで行われます。
+//
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+
+    static bool check(const string& sq_str, int value, int offset) {
+        if (size(sq_str) <= offset) {
+            return value == 0;
+        }
+        for (int len = 1; len <= size(sq_str) - offset; len++) {
+            const auto cur_num = sq_str.substr(offset, len);
+            const auto cur_value = stoi(cur_num);
+            if (cur_value <= value) {
+                if (check(sq_str, value - cur_value, offset + len)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    static bool check(int value) {
+        const auto sq = value * value;
+        const auto sq_str = to_string(sq);
+        return check(sq_str, value, 0);
+    }
+
+    int punishmentNumber(int n) {
+        int total = 0;
+        for (int i = 1; i <= n; i++) {
+            if (check(i)) {
+                total += i * i;
+            }
+        }
+        return total;
+    }
+};
+
+void test(int n) {
+    cout << Solution().punishmentNumber(n) << endl;
+}
+
+int main()
+{
+    test(10);
+    test(37);
+    test(1000);
+    return 0;
+}
