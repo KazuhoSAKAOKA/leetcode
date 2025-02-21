c186def3505190a9708333220f4b124fd01c1638
Kazuho Sakaoka <kazuho-sakaoka@outlook.jp>
2025-02-21 23:39:14 +0900

    Q1261

---
+++ leetcode_Q1261/leetcode_Q1261.cpp
@@ -0,0 +1,49 @@
+﻿// leetcode_Q1261.cpp : このファイルには 'main' 関数が含まれています。プログラム実行の開始と終了がそこで行われます。
+//
+
+#include <iostream>
+#include <vector>
+#include <set>
+#include "./../utility/leetcode_testcase_helper.h"
+#include "./../utility/TreeNodeUtil.h"
+
+using namespace std;
+class FindElements {
+public:
+
+    static void rec(TreeNode* node, int current, set<int>& memo) {
+        if (node == nullptr) {
+            return;
+        }
+        node->val = current;
+        memo.insert(current);
+        rec(node->left, current * 2 + 1, memo);
+        rec(node->right, current * 2 + 2, memo);
+    }
+
+    set<int> memo;
+    FindElements(TreeNode* root) {
+        rec(root, 0, memo);
+    }
+
+    bool find(int target) {
+        return memo.count(target) != 0;
+    }
+};
+
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
+// プログラムの実行: Ctrl + F5 または [デバッグ] > [デバッグなしで開始] メニュー
+// プログラムのデバッグ: F5 または [デバッグ] > [デバッグの開始] メニュー
+
+// 作業を開始するためのヒント: 
+//    1. ソリューション エクスプローラー ウィンドウを使用してファイルを追加/管理します 
+//   2. チーム エクスプローラー ウィンドウを使用してソース管理に接続します
+//   3. 出力ウィンドウを使用して、ビルド出力とその他のメッセージを表示します
+//   4. エラー一覧ウィンドウを使用してエラーを表示します
+//   5. [プロジェクト] > [新しい項目の追加] と移動して新しいコード ファイルを作成するか、[プロジェクト] > [既存の項目の追加] と移動して既存のコード ファイルをプロジェクトに追加します
+//   6. 後ほどこのプロジェクトを再び開く場合、[ファイル] > [開く] > [プロジェクト] と移動して .sln ファイルを選択します
