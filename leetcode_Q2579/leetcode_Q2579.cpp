c5e1d42d181cc4e9a7d6cc91b169fec0b10567f1
Kazuho Sakaoka <kazuho-sakaoka@outlook.jp>
2025-03-05 20:04:13 +0900

    Q2579

---
+++ leetcode_Q2579/leetcode_Q2579.cpp
@@ -0,0 +1,24 @@
+﻿// leetcode_Q2579.cpp : このファイルには 'main' 関数が含まれています。プログラム実行の開始と終了がそこで行われます。
+//
+
+#include <iostream>
+using namespace std;
+class Solution {
+public:
+    long long coloredCells(int n) {
+        const long long nl = static_cast<long long>(n);
+        const long long sq = ((nl - 1) * 2 + 1);
+        return sq * sq - 4LL * n * (n - 1LL) / 2LL;
+    }
+};
+
+void test(int n) {
+    cout << Solution().coloredCells(n) << endl;
+}
+int main()
+{
+    test(1);
+    test(2);
+    test(3);
+    return 0;
+}
