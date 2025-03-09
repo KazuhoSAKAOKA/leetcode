f52ec928ee098834a7803f1589b37791a7f1d6ca
Kazuho Sakaoka <kazuho-sakaoka@outlook.jp>
2025-03-09 11:11:18 +0900

    Q1100

---
+++ leetcode_Q1100/leetcode_Q1100.cpp
@@ -0,0 +1,50 @@
+﻿// leetcode_Q1100.cpp : このファイルには 'main' 関数が含まれています。プログラム実行の開始と終了がそこで行われます。
+//
+
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+class Solution {
+public:
+    int numKLenSubstrNoRepeats(string s, int k) {
+        vector<int> freqs(26);
+        int repeat_count = 0;
+        int count = 0;
+        for (int i = 0; i < k - 1; i++) {
+            freqs[s[i] - 'a']++;
+            if (freqs[s[i] - 'a'] == 2) {
+                repeat_count++;
+            }
+        }
+
+        for (int i = k - 1; i < size(s); i++) {
+            freqs[s[i] - 'a']++;
+            if (freqs[s[i] - 'a'] == 2) {
+                repeat_count++;
+            }
+            if (repeat_count == 0) {
+                count++;
+            }
+            const int j = i - k + 1;
+            freqs[s[j] - 'a']--;
+            if (freqs[s[j] - 'a'] == 1) {
+                repeat_count--;
+            }
+        }
+
+        return count;
+    }
+};
+
+void test(string&& s, int k) {
+    cout << Solution().numKLenSubstrNoRepeats(s, k) << endl;
+}
+
+int main()
+{
+    test("havefunonleetcode", 5);
+    test("home", 5);
+    return 0;
+}
