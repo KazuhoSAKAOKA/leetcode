570cc62ae6f8dc39329ff63cf2de1f411ee15c7f
Kazuho Sakaoka <kazuho-sakaoka@outlook.jp>
2025-04-13 11:03:17 +0900

    Q323

---
+++ leetcode_Q0323/main.cpp
@@ -0,0 +1,78 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <numeric>
+#include <climits>
+#include <cmath>
+
+using namespace std;
+class Solution {
+
+
+    struct union_find {
+
+        std::vector<size_t> parents_;
+        std::vector<size_t> sizes_;
+
+        union_find(size_t n) : parents_(n, 0), sizes_(n, 1) {
+            for (size_t i = 0; i < n; i++) {
+                parents_[i] = i;
+            }
+        }
+
+        size_t root(size_t x) {
+            if (x == parents_[x]) { return x; }
+            parents_[x] = root(parents_[x]);
+            return parents_[x];
+        }
+
+        bool same(size_t x, size_t y) {
+            return root(x) == root(y);
+        }
+
+        void unite(size_t x, size_t y) {
+            const auto rootx = root(x);
+            const auto rooty = root(y);
+            if (rootx == rooty) { return; }
+            if (sizes_[rootx] < sizes_[rooty]) {
+                parents_[rootx] = rooty;
+                sizes_[rooty] += sizes_[rootx];
+            }
+            else {
+                parents_[rooty] = rootx;
+                sizes_[rootx] += sizes_[rooty];
+            }
+        }
+
+        size_t size(size_t x) {
+            return sizes_[root(x)];
+        }
+
+        bool is_root(size_t x) {
+            return root(x) == x;
+        }
+    };
+
+public:
+
+
+    int countComponents(int n, vector<vector<int>>& edges) {
+        union_find uf(n);
+        for (auto&& edge : edges) {
+            uf.unite(edge.front(), edge.back());
+        }
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            if (uf.is_root(i)) {
+                count++;
+            }
+        }
+        return count;
+    }
+};
+int main() {
+	return 0;
+}
