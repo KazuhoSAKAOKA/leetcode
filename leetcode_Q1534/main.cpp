71f4ae76ecce3ac7a05eb2cc64eabded58d7d96b
Kazuho Sakaoka <kazuho-sakaoka@outlook.jp>
2025-04-14 23:51:07 +0900

    Q1534

---
+++ leetcode_Q1534/main.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <numeric>
+#include <climits>
+#include <cmath>
+
+using namespace std;
+class Solution {
+public:
+    int countGoodTriplets(vector<int>& arr, int a, int b, int c) {
+        int count = 0;
+        const auto n = size(arr);
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+
+                const auto diffa = abs(arr[i] - arr[j]);
+                if (diffa <= a) {
+                    for (int k = j + 1; k < n; k++) {
+                        const auto diffb = abs(arr[j] - arr[k]);
+                        const auto diffc = abs(arr[i] - arr[k]);
+                        if (diffb <= b && diffc <= c) {
+                            count++;
+                        }
+                    }
+                }
+            }
+        }
+       
+        return count;
+    }
+};
+int main() {
+	return 0;
+}
