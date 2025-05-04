8d42c0e0f52d048fb7862ddea5e80e3cd31fd3d5
Kazuho Sakaoka <kazuho-sakaoka@outlook.jp>
2025-05-04 10:40:21 +0900

    Q1128

---
+++ leetcode_Q1128/main.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <numeric>
+#include <climits>
+#include <cmath>
+
+using namespace std;
+class Solution {
+public:
+    int numEquivDominoPairs(vector<vector<int>>& dominoes) {
+        map<pair<int, int>, int> freqs;
+        int total_count = 0;
+        for (auto&& domino : dominoes) {
+            const pair<int, int> key = { min(domino[0], domino[1]), max(domino[0], domino[1]) };
+            const auto it = freqs.find(key);
+            if (it != cend(freqs)) {
+                total_count += it->second;
+                it->second++;
+            }
+            else {
+                freqs.insert({key, 1});
+            }
+        }
+        return total_count;
+    }
+};
+
+
+int main() {
+	return 0;
+}
