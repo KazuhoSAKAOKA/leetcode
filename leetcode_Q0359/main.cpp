3eff3d8901f4e29b9da343134b5609fffbba2a11
Kazuho Sakaoka <kazuho-sakaoka@outlook.jp>
2025-05-12 20:15:30 +0900

    Q359

---
+++ leetcode_Q0359/main.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <numeric>
+#include <climits>
+#include <cmath>
+#include <unordered_set>
+using namespace std;
+class Logger {
+private:
+    map<int, unordered_set<string>> exists_;
+
+
+public:
+    Logger() {
+
+    }
+
+    bool shouldPrintMessage(int timestamp, string message) {
+        const int limit = timestamp - 10;
+        auto it = begin(exists_);
+        while (it != end(exists_) && it->first <= limit) {
+            it = exists_.erase(it);
+        }
+
+        while (it != end(exists_)) {
+            if (it->second.find(message) != end(it->second)) {
+                return false;
+            }
+            ++it;
+        }
+        exists_[timestamp].insert(message);
+        return true;
+    }
+};
+
+int main() {
+	return 0;
+}
