318457556f42791c94b35f41565176643e54abd0
Kazuho Sakaoka <kazuho-sakaoka@outlook.jp>
2025-05-19 18:57:41 +0900

    Q3024

---
+++ leetcode_Q3024/main.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <numeric>
+#include <climits>
+#include <cmath>
+#include "./../utility/leetcode_testcase_helper.h"
+
+using namespace std;
+class Solution {
+public:
+    string triangleType(vector<int>& nums) {
+        sort(begin(nums), end(nums));
+        if (nums[0] + nums[1] <= nums[2]) {
+            return "none";
+        }
+        if (nums[0] == nums[1] && nums[1] == nums[2]) {
+            return "equilateral";
+        }
+        else if(nums[0] == nums[1] || nums[1] == nums[2]){
+            return "isosceles";
+        }
+        return "scalene";
+    }
+};
+
+void test(vector<int>&& nums) {
+    cout << Solution().triangleType(nums) << endl;
+}
+
+int main() {
+	return 0;
+}
