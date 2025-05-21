330f944cdf4f9af273f2cb92544281aa48a56253
Kazuho Sakaoka <kazuho-sakaoka@outlook.jp>
2025-05-21 21:51:58 +0900

    Q73

--- leetcode_Q0073/leetcode_Q0073/leetcode_Q0073.cpp
+++ leetcode_Q0073/leetcode_Q0073/leetcode_Q0073.cpp
@@ -7,7 +7,8 @@ using namespace std;
 
 class Solution {
 public:
-    void setZeroes(vector<vector<int>>& matrix) {
+
+    static void ignore(vector<vector<int>>& matrix) {
         vector<bool> rows(matrix.size(), false);
         vector<bool> cols(matrix.front().size(), false);
 
@@ -35,6 +36,59 @@ public:
             }
         }
     }
+
+    void setZeroes(vector<vector<int>>& matrix) {
+        const auto m = size(matrix.front());
+        const auto n = size(matrix);
+        bool iszero_first_row = false;
+        for (int j = 0; j < m; j++) {
+            if (matrix[0][j] == 0) {
+                iszero_first_row = true;
+                break;
+            }
+        }
+        bool iszero_first_col = false;
+        for (int i = 0; i < n; i++) {
+            if (matrix[i][0] == 0) {
+                iszero_first_col = true;
+                break;
+            }
+        }
+        for (int i = 1; i < n; i++) {
+            for (int j = 1; j < m; j++) {
+                if (matrix[i][j] == 0) {
+                    matrix[0][j] = 0;
+                    matrix[i][0] = 0;
+                }
+            }
+        }
+
+        for (int i = 1; i < n; i++) {
+            if (matrix[i][0] == 0) {
+                for (int j = 0; j < m; j++) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+        for (int j = 1; j < m; j++) {
+            if (matrix[0][j] == 0) {
+                for (int i = 0; i < n; i++) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
+        if (iszero_first_row) {
+            for (int j = 0; j < m; j++) {
+                matrix[0][j] = 0;
+            }
+        }
+        if (iszero_first_col) {
+            for (int i = 0; i < n; i++) {
+                matrix[i][0] = 0;
+            }
+        }
+    }
 };
 
 void test(vector<vector<int>>&& matrix) {
@@ -51,7 +105,9 @@ void test(vector<vector<int>>&& matrix) {
 
 int main()
 {
-    test({ {1, 1, 1}, { 1,0,1 }, { 1,1,1 } });
+    test({ {0,0,0,5},{4,3,1,4},{0,1,1,4},{1,2,1,3},{0,0,1,1} });
     test({ {0, 1, 2, 0}, { 3,4,5,2 }, { 1,3,1,5 } });
+    test({ {1, 2, 3, 4}, { 5,0,7,8 }, {0,10,11,12 },{13,14,15,0}, });
+    test({ {1, 1, 1}, { 1,0,1 }, { 1,1,1 } });
 
 }
