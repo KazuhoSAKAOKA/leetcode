6be1ca7da8a837122d20c8f5b7ff4196122b1115
Kazuho Sakaoka <kazuho-sakaoka@outlook.jp>
2025-05-24 18:07:54 +0900

    Q2942

---
+++ leetcode_Q2942/main.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <numeric>
+#include <climits>
+#include <cmath>
+
+using namespace std;
+class Solution {
+public:
+    vector<int> findWordsContaining(vector<string>& words, char x) {
+        vector<int> ans;
+        for (int i = 0; i < size(words); i++) {
+            if (words[i].find(x) != string::npos) {
+                ans.emplace_back(i);
+            }
+        }
+        return ans;
+    }
+};
+
+int main() {
+	return 0;
+}
