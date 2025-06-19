c9b648b04d05f1f0503623f57665322945e7dcc1
Kazuho Sakaoka <kazuho-sakaoka@outlook.jp>
2025-06-19 19:10:07 +0900

    Q2294

---
+++ leetcode_Q2294/main.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <numeric>
+#include <climits>
+#include <cmath>
+
+using namespace std;
+class Solution {
+public:
+    int partitionArray(vector<int>& nums, int k) {
+        sort(begin(nums), end(nums));
+        int count = 0;
+        auto cur_it = cbegin(nums);
+        while (cur_it != cend(nums)) {
+            count++;
+            const auto limit = *cur_it + k;
+            cur_it = upper_bound(cur_it, cend(nums), limit);
+        }
+        return count;
+    }
+};
+
+void test(vector<int>&& nums, int k) {
+    cout << Solution().partitionArray(nums, k) << endl;
+}
+int main() {
+
+	return 0;
+}
