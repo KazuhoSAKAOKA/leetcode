5eb8129d02204e7a2e70c099ddd2dea43c4eedea
Kazuho Sakaoka <kazuho-sakaoka@outlook.jp>
2025-09-30 06:39:41 +0900

    Q1121

---
+++ leetcode_Q1121/main.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <numeric>
+#include <climits>
+#include <cmath>
+#include <unordered_map>
+using namespace std;
+class Solution {
+public:
+    bool canDivideIntoSubsequences(vector<int>& nums, int k) {
+        unordered_map<int, int> freq;
+        int max_freq = 0;
+        for (auto&& num : nums) {
+            freq[num]++;
+            max_freq = max(max_freq, freq[num]);
+        }
+        return max_freq * k <= size(nums);
+    }
+};
+
+static void test(vector<int>&& nums, int k) {
+
+}
+
+int main() {
+	return 0;
+}
