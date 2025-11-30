99823ae62ea1975601a385c368903f3253e85558
Kazuho Sakaoka <kazuho-sakaoka@outlook.jp>
2025-11-30 22:17:34 +0900

    Q1590

--- leetcode_Q1590/leetcode_Q1590.cpp
+++ leetcode_Q1590/leetcode_Q1590.cpp
@@ -3,7 +3,10 @@
 
 #include <iostream>
 #include <vector>
-
+#include <map>
+#include <algorithm>
+#include <numeric>
+#include <fstream>
 #include "./../utility/leetcode_testcase_helper.h"
 
 using namespace std;
@@ -11,21 +14,36 @@ using namespace std;
 class Solution {
 public:
     int minSubarray(vector<int>& nums, int p) {
-        vector<long long> prefix_sums(size(nums) + 1);
-        for (size_t i = 0; i < size(nums); i++) {
-            prefix_sums[i + 1] = prefix_sums[i] + static_cast<long long>(nums[i]);
+        const long long total = accumulate(cbegin(nums), cend(nums), 0LL, [](long long t, int v)->long long {  return t + v; });
+        if (total < p) {
+            return -1;
         }
-        if (prefix_sums[size(nums)] % p == 0) {
+        const long long total_mod = total % p;
+        if (total_mod == 0) {
             return 0;
         }
-        for (size_t len = 1; len < size(nums); len++) {
-            for (size_t i = 0; i < size(nums) - len + 1; i++) {
-                if ((prefix_sums[size(nums)] - (prefix_sums[i + len] - prefix_sums[i])) % p == 0) {
-                    return static_cast<int>(len);
-                }
+        long long prefix_sum = 0LL;
+        map<int, int> mp{ { 0,0 } };
+        int min_len = static_cast<int>(size(nums));
+        for (int i = 0; i < size(nums); i++) {
+            if (nums[i] == total_mod) {
+                min_len = 1;
+                break;
             }
+            prefix_sum += nums[i];
+            const auto r = prefix_sum % p;
+            const auto target = (r - total_mod + p) % p;
+            const auto it = mp.find(target);
+            if (it != cend(mp)) {
+                min_len = min(min_len, i + 1 - it->second);
+            }
+            mp[r] = i + 1;
         }
-        return -1;
+
+        if (min_len == static_cast<int>(size(nums))) {
+            return -1;
+        }
+        return min_len;
     }
 };
 
@@ -34,8 +52,10 @@ void test(vector<int>&& nums, int p) {
 }
 int main()
 {
-    test(get_list_int("[3,1,4,2]"), 6);
+    test(get_list_int("[3,6,8,1]"), 8);
+    test(get_list_int("[4,4,2]"), 7);
     test(get_list_int("[6,3,5,2]"), 9);
+    test(get_list_int("[3,1,4,2]"), 6);
     test(get_list_int("[1,2,3]"), 3);
     return 0;
 }
