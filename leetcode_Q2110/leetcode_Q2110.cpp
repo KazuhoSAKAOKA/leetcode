1c9d60635d26e6d59497dfd16633fc83631ca46a
Kazuho Sakaoka <kazuho-sakaoka@outlook.jp>
2025-12-15 22:14:39 +0900

    Q2110

---
+++ leetcode_Q2110/leetcode_Q2110.cpp
@@ -0,0 +1,38 @@
+﻿// leetcode_Q2110.cpp : このファイルには 'main' 関数が含まれています。プログラム実行の開始と終了がそこで行われます。
+//
+
+#include <iostream>
+#include <vector>
+#include "./../utility/leetcode_testcase_helper.h"
+
+using namespace std;
+class Solution {
+public:
+    long long getDescentPeriods(vector<int>& prices) {
+        const auto n = size(prices);
+        long long count = 1LL;
+        long long l = 0;
+        for (long long r = 1; r < n; r++) {
+            if (prices[r - 1] - 1 != prices[r]) {
+                l = r;
+            }
+            count += r - l + 1LL;
+        }
+        return count;
+    }
+};
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
+// プログラムの実行: Ctrl + F5 または [デバッグ] > [デバッグなしで開始] メニュー
+// プログラムのデバッグ: F5 または [デバッグ] > [デバッグの開始] メニュー
+
+// 作業を開始するためのヒント: 
+//    1. ソリューション エクスプローラー ウィンドウを使用してファイルを追加/管理します 
+//   2. チーム エクスプローラー ウィンドウを使用してソース管理に接続します
+//   3. 出力ウィンドウを使用して、ビルド出力とその他のメッセージを表示します
+//   4. エラー一覧ウィンドウを使用してエラーを表示します
+//   5. [プロジェクト] > [新しい項目の追加] と移動して新しいコード ファイルを作成するか、[プロジェクト] > [既存の項目の追加] と移動して既存のコード ファイルをプロジェクトに追加します
+//   6. 後ほどこのプロジェクトを再び開く場合、[ファイル] > [開く] > [プロジェクト] と移動して .sln ファイルを選択します
