51062cce103b80f176e960df27e6019157505a18
Kazuho Sakaoka <kazuho-sakaoka@outlook.jp>
2026-01-27 22:03:38 +0900

    Q3650

---
+++ leetcode_Q3650/leetcode_Q3650.cpp
@@ -0,0 +1,83 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+#include <stack>
+#include <queue>
+#include <algorithm>
+#include <numeric>
+#include <cmath>
+#include <climits>
+#include <tuple>
+#include <functional>
+#include <bit>
+#include <exception>
+#include "./../utility/leetcode_testcase_helper.h"
+using namespace std;
+
+class Solution {
+public:
+
+	template <typename T>
+	static T djikstra(const std::vector<std::vector<pair<int, int>>>& graph, int s, int e1) {
+		std::vector<T> costs(graph.size(), std::numeric_limits<T>::max());
+		struct point {
+			T cost;
+			int pos;
+			bool operator > (const point& other) const {
+				return cost > other.cost;
+			}
+		};
+		std::priority_queue<point, std::vector<point>, std::greater<point>> p_queue;
+		p_queue.push(point{ 0, s });
+		costs[s] = 0;
+		while (!p_queue.empty()) {
+			const auto [current_cost, current] = p_queue.top();
+			p_queue.pop();
+			if (costs[current] < current_cost) { continue; }
+			for (auto&& [to, cost] : graph[current]) {
+				if (costs[to] > cost + current_cost) {
+					costs[to] = cost + current_cost;
+					p_queue.push(point{ costs[to] , to });
+				}
+			}
+		}
+		return costs[e1];
+	}
+
+
+	int minCost(int n, vector<vector<int>>& edges) {
+
+		vector<vector<pair<int, int>>> graph(n);
+		for (auto&& edge : edges) {
+			const auto u = edge[0];
+			const auto v = edge[1];
+			const auto w = edge[2];
+			graph[u].push_back({ v, w });
+			graph[v].push_back({ u, w * 2 });
+		}
+
+		const auto cost = djikstra<int>(graph, 0, n - 1);
+		if (cost == std::numeric_limits<int>::max()) { return -1; }
+		return cost;
+	}
+};
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
+// プログラムの実行: Ctrl + F5 または [デバッグ] > [デバッグなしで開始] メニュー
+// プログラムのデバッグ: F5 または [デバッグ] > [デバッグの開始] メニュー
+
+// 作業を開始するためのヒント: 
+//    1. ソリューション エクスプローラー ウィンドウを使用してファイルを追加/管理します 
+//   2. チーム エクスプローラー ウィンドウを使用してソース管理に接続します
+//   3. 出力ウィンドウを使用して、ビルド出力とその他のメッセージを表示します
+//   4. エラー一覧ウィンドウを使用してエラーを表示します
+//   5. [プロジェクト] > [新しい項目の追加] と移動して新しいコード ファイルを作成するか、[プロジェクト] > [既存の項目の追加] と移動して既存のコード ファイルをプロジェクトに追加します
+//   6. 後ほどこのプロジェクトを再び開く場合、[ファイル] > [開く] > [プロジェクト] と移動して .sln ファイルを選択します
