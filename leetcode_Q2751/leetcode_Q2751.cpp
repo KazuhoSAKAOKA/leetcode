ab3b3b13b93788979af213068ecc8e0262f5512f
Kazuho Sakaoka <kazuho-sakaoka@outlook.jp>
2026-04-01 22:00:59 +0900

    Q2751 fix

--- leetcode_Q2751/leetcode_Q2751.cpp
+++ leetcode_Q2751/leetcode_Q2751.cpp
@@ -5,6 +5,7 @@
 #include <vector>
 #include <string>
 #include <map>
+#include <algorithm>
 
 #include "./../utility/leetcode_testcase_helper.h"
 
@@ -12,7 +13,7 @@ using namespace std;
 
 class Solution {
 public:
-    vector<int> survivedRobotsHealths(vector<int>& positions, vector<int>& healths, string directions) {
+    static vector<int> past_survivedRobotsHealths(vector<int>& positions, vector<int>& healths, string directions) {
         map<int, size_t> left_dirs;
         map<int, size_t> right_dirs;
 
@@ -61,6 +62,39 @@ public:
             if (health > 0) {
                 ans.emplace_back(health);
             }
+        }
+        return ans;
+    }
+
+    vector<int> survivedRobotsHealths(vector<int>& positions, vector<int>& healths, string directions) {
+        const auto n = size(positions);
+        vector<size_t> indexes(n);
+        for (size_t i = 0; i < n; i++) { indexes[i] = i; }
+        sort(begin(indexes), end(indexes), [&](size_t a, size_t b) { return positions[a] < positions[b]; });
+
+        vector<size_t> stk;
+        for (auto&& index : indexes) {
+            stk.push_back(index);
+            while (size(stk) > 1 && directions[stk[size(stk) - 2]] == 'R' && directions[stk[size(stk) - 1]] == 'L') {
+                if (healths[stk[size(stk) - 2]] == healths[stk[size(stk) - 1]]) {
+                    stk.pop_back();
+                    stk.pop_back();
+                }
+                else if (healths[stk[size(stk) - 2]] > healths[stk[size(stk) - 1]]) {
+                    stk.pop_back();
+                    healths[stk[size(stk) - 1]]--;
+                }
+                else {
+                    stk[size(stk) - 2] = stk[size(stk) - 1];
+                    stk.pop_back();
+                    healths[stk[size(stk) - 1]]--;
+                }
+            }
+        }
+        sort(begin(stk), end(stk));
+        vector<int> ans(size(stk));
+        for (size_t i = 0; i < size(stk); i++) {
+            ans[i] = healths[stk[i]];
         }
         return ans;
     }
